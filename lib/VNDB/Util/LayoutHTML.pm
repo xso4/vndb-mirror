e0fdfb55a21eecae700c969c728b5ea0fef2cf45
Yorhel <git@yorhel.nl>
2021-01-04 14:09:52 +0100

    VNDB::Util::LayoutHTML: Fix reference to config params in old code
    Turns out I forgot something in 3fc190f1898342d63b1308a83152fe45a88f7c3f

--- lib/VNDB/Util/LayoutHTML.pm
+++ lib/VNDB/Util/LayoutHTML.pm
@@ -4,6 +4,7 @@ package VNDB::Util::LayoutHTML;
 use strict;
 use warnings;
 use TUWF ':html';
+use VNDB::Config;
 use VNWeb::HTML;
 use Exporter 'import';
 
@@ -34,7 +35,7 @@ sub htmlFooter { # %options => { pref_code => 1 }
     # Abuse an empty noscript tag for the formcode to update a preference setting, if the page requires one.
     noscript id => 'pref_code', title => $self->authGetCode('/xml/prefs.xml'), ''
       if $o{pref_code} && $self->authInfo->{id};
-    script type => 'text/javascript', src => $self->{url_static}.'/f/vndb.js?'.$self->{version}, '';
+    script type => 'text/javascript', src => config->{url_static}.'/f/vndb.js?'.config->{version}, '';
     VNWeb::HTML::_scripts_({});
    end 'body';
   end 'html';
