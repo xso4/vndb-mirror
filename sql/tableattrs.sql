db621ea9e1c827735bef546e08d15a69e50b81d1
Yorhel <git@yorhel.nl>
2021-01-29 11:37:10 +0100

    SQL: Add index on releases.released to speed up homepage queries
    Last time I tried this in PostgreSQL 11 the index would only get used
    for one of the two queries, now it properly works on both and gives a
    nice ~25ms boost.

--- sql/tableattrs.sql
+++ sql/tableattrs.sql
@@ -121,6 +121,7 @@ CREATE        INDEX chars_image            ON chars (image);
 CREATE UNIQUE INDEX image_votes_pkey       ON image_votes (uid, id);
 CREATE        INDEX image_votes_id         ON image_votes (id);
 CREATE        INDEX notifications_uid_iid  ON notifications (uid,iid);
+CREATE        INDEX releases_released      ON releases (released) WHERE NOT hidden; -- Mainly for the homepage
 CREATE        INDEX releases_producers_pid ON releases_producers (pid);
 CREATE        INDEX releases_vn_vid        ON releases_vn (vid);
 CREATE        INDEX reports_new            ON reports (date) WHERE status = 'new';
