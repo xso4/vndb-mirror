1e140e05f578bedf840d35b29a3e544e27f02abe
Yorhel <git@yorhel.nl>
2021-02-20 14:40:15 +0100

    Chars::List: Add experimental card layout

--- lib/VNWeb/Chars/List.pm
+++ lib/VNWeb/Chars/List.pm
@@ -3,6 +3,7 @@ package VNWeb::Chars::List;
 use VNWeb::Prelude;
 use VNWeb::AdvSearch;
 use VNWeb::Filters;
+use VNWeb::Images::Lib;
 
 
 # Also used by VNWeb::TT::TraitPage
@@ -10,6 +11,7 @@ sub listing_ {
     my($opt, $list, $count) = @_;
     my sub url { '?'.query_encode %$opt, @_ }
     paginate_ \&url, $opt->{p}, [$count, 50], 't';
+
     div_ class => 'mainbox browse charb', sub {
         table_ class => 'stripe', sub {
             tr_ sub {
@@ -24,7 +26,30 @@ sub listing_ {
                 };
             } for @$list;
         }
-    };
+    } if !$opt->{card};
+
+    div_ class => 'mainbox charbcard', sub {
+        my($w,$h) = (80,100);
+        div_ sub {
+            div_ sub {
+                if($_->{image}) {
+                    my($iw,$ih) = imgsize $_->{image}{width}*100, $_->{image}{height}*100, $w, $h;
+                    image_ $_->{image}, alt => $_->{name}, width => $iw, height => $ih, url => "/c$_->{id}";
+                } else {
+                    txt_ 'no image';
+                }
+            };
+            div_ sub {
+                abbr_ class => "icons gen $_->{gender}", title => $GENDER{$_->{gender}}, '' if $_->{gender} ne 'unknown';
+                a_ href => "/c$_->{id}", title => $_->{original}||$_->{name}, $_->{name};
+                br_;
+                b_ class => 'grayedout', sub {
+                    join_ ', ', sub { a_ href => "/v$_->{id}", title => $_->{original}||$_->{title}, $_->{title} }, $_->{vn}->@*;
+                };
+            };
+        } for @$list;
+    } if $opt->{card};
+
     paginate_ \&url, $opt->{p}, [$count, 50], 'b';
 }
 
@@ -48,6 +73,7 @@ TUWF::get qr{/c(?:/(?<char>all|[a-z0]))?}, sub {
         f => { advsearch_err => 'c' },
         ch=> { onerror => [], type => 'array', scalar => 1, values => { onerror => undef, enum => ['0', 'a'..'z'] } },
         fil => { required => 0 },
+        card => { anybool => 1 }, # XXX: Experimental option, will be consolidated into a merged "display settings" field
     )->data;
     $opt->{ch} = $opt->{ch}[0];
 
@@ -81,11 +107,12 @@ TUWF::get qr{/c(?:/(?<char>all|[a-z0]))?}, sub {
     db_maytimeout {
         $count = tuwf->dbVali('SELECT count(*) FROM chars c WHERE', $where);
         $list = $count ? tuwf->dbPagei({results => 50, page => $opt->{p}}, '
-            SELECT c.id, c.name, c.original, c.gender FROM chars c WHERE', $where, 'ORDER BY c.name, c.id'
+            SELECT c.id, c.name, c.original, c.gender, c.image FROM chars c WHERE', $where, 'ORDER BY c.name, c.id'
         ) : [];
     } || (($count, $list) = (undef, []));
 
     enrich_listing $list;
+    enrich_image_obj image => $list;
     $time = time - $time;
 
     framework_ title => 'Browse characters', sub {
