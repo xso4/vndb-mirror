82267a30fd0e9d46e7be76204f34c232b438b874
Yorhel <git@yorhel.nl>
2021-08-03 11:01:00 +0200

    VNLengthVote: Fix migration script + no need to query DB when !$canvote

--- util/updates/2021-08-03-vnlength-speed.sql
+++ util/updates/2021-08-03-vnlength-speed.sql
@@ -1,4 +1,4 @@
-ALTER TABLE vn_length_votes ADD COLUMN speed smallint NOT NULL;
+ALTER TABLE vn_length_votes ADD COLUMN speed smallint NOT NULL DEFAULT 0;
 ALTER TABLE vn_length_votes ALTER COLUMN speed DROP DEFAULT;
 ALTER TABLE vn_length_votes ADD COLUMN notes2 text NOT NULL DEFAULT '';
 UPDATE vn_length_votes SET notes2 = notes;
