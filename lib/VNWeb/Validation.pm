xso4/vndb-mirror

commit 8c1fcdb94b46373c4e9c8d533e645edf6aba6f7a

Add mod option to set deletion reason for forum posts

lib/VNWeb/Validation.pm
@@ -245,7 +245,7 @@ sub can_edit {
         } else {
             die "Can't do authorization test when hidden/date/user_id fields aren't present"
                 if !exists $entry->{hidden} || !exists $entry->{date} || !exists $entry->{user_id};
-            return auth && $entry->{user_id} eq auth->uid && !$entry->{hidden} && $entry->{date} > time-config->{board_edit_time};
+            return auth && $entry->{user_id} eq auth->uid && !defined $entry->{hidden} && $entry->{date} > time-config->{board_edit_time};
         }
     }
 

elm/Discussions/PostEdit.elm
@@ -25,7 +25,7 @@ type alias Model =
   , id          : String
   , num         : Int
   , can_mod     : Bool
-  , hidden      : Bool
+  , hidden      : Maybe String
   , nolastmod   : Bool
   , delete      : Bool
   , msg         : TP.Model
@@ -56,7 +56,7 @@ encode m =
 
 
 type Msg
-  = Hidden Bool
+  = Hidden (Maybe String)
   | Nolastmod Bool
   | Delete Bool
   | Content TP.Msg
@@ -67,9 +67,9 @@ type Msg
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    Hidden  b     -> ({ model | hidden  = b }, Cmd.none)
-    Nolastmod b   -> ({ model | nolastmod=b }, Cmd.none)
-    Delete  b     -> ({ model | delete  = b }, Cmd.none)
+    Hidden  s     -> ({ model | hidden    = s }, Cmd.none)
+    Nolastmod b   -> ({ model | nolastmod = b }, Cmd.none)
+    Delete  b     -> ({ model | delete    = b }, Cmd.none)
     Content m     -> let (nm,nc) = TP.update m model.msg in ({ model | msg = nm }, Cmd.map Content nc)
 
     Submit -> ({ model | state = Api.Loading }, GPE.send (encode model) Submitted)
@@ -85,7 +85,12 @@ view model =
     , table [ class "formtable" ] <|
       [ formField "Post" [ a [ href <| "/" ++ model.id ++ "." ++ String.fromInt model.num ] [ text <| "#" ++ String.fromInt model.num ++ " on " ++ model.id ] ]
       , if model.can_mod
-        then formField "" [ label [] [ inputCheck "" model.hidden Hidden, text " Hidden" ] ]
+        then formField ""
+        [ label [] [ inputCheck "" (model.hidden /= Nothing) (\b -> Hidden (if b then Just "" else Nothing)), text " Hidden" ]
+        , Maybe.withDefault (text "") <| Maybe.map (\msg ->
+            span [] [ br [] [], inputText "" msg (Just >> Hidden) [placeholder "(Optional) reason for deletion", style "width" "500px"] ]
+          ) model.hidden
+        ]
         else text ""
       , if model.can_mod
         then formField "" [ label [] [ inputCheck "" model.nolastmod Nolastmod, text " Don't update last modification timestamp" ] ]

lib/VNWeb/Discussions/PostEdit.pm
@@ -10,7 +10,7 @@ my $FORM = {
     num         => { id => 1 },
 
     can_mod     => { anybool => 1, _when => 'out' },
-    hidden      => { anybool => 1 }, # When can_mod
+    hidden      => { required => 0 }, # When can_mod
     nolastmod   => { anybool => 1, _when => 'in' }, # When can_mod
     delete      => { anybool => 1 }, # When can_mod
 
@@ -44,7 +44,7 @@ elm_api DiscussionsPostEdit => $FORM_OUT, $FORM_IN, sub {
     return tuwf->resNotFound if !$t->{id};
     return elm_Unauth if !can_edit t => $t;
 
-    tuwf->dbExeci(q{DELETE FROM notifications WHERE iid =}, \$id, 'AND num =', \$num) if auth->permBoardmod && ($data->{delete} || $data->{hidden});
+    tuwf->dbExeci(q{DELETE FROM notifications WHERE iid =}, \$id, 'AND num =', \$num) if auth->permBoardmod && ($data->{delete} || defined $data->{hidden});
 
     if($data->{delete} && auth->permBoardmod) {
         auth->audit($t->{user_id}, 'post delete', "deleted $id.$num");

lib/VNWeb/Discussions/Search.pm
@@ -78,7 +78,7 @@ sub posts_ {
           FROM threads_posts tp
           JOIN threads t ON t.id = tp.tid
           LEFT JOIN users u ON u.id = tp.uid
-         WHERE NOT t.hidden AND NOT t.private AND NOT tp.hidden
+         WHERE NOT t.hidden AND NOT t.private AND tp.hidden IS NULL
            AND bb_tsvector(tp.msg) @@ to_tsquery(}, \$ts, ')',
                $filt->{b}->@* < keys %BOARD_TYPE ? ('AND t.id IN(SELECT tid FROM threads_boards WHERE type IN', $filt->{b}, ')') : (), q{
          ORDER BY tp.date DESC

lib/VNWeb/Discussions/Thread.pm
@@ -102,10 +102,10 @@ sub posts_ {
     paginate_ \&url, $page, [ $t->{count}, 25 ], 't';
     div_ class => 'mainbox thread', id => 'threadstart', sub {
         table_ class => 'stripe', sub {
-            tr_ mkclass(deleted => $_->{hidden}), id => $_->{num}, sub {
+            tr_ mkclass(deleted => defined $_->{hidden}), id => $_->{num}, sub {
                 td_ class => 'tc1', $_ == $posts->[$#$posts] ? (id => 'last') : (), sub {
                     a_ href => "/$t->{id}.$_->{num}", "#$_->{num}";
-                    if(!$_->{hidden} || auth->permBoard) {
+                    if(!defined $_->{hidden} || auth->permBoard) {
                         txt_ ' by ';
                         user_ $_;
                         br_;
@@ -121,9 +121,12 @@ sub posts_ {
                         }
                         a_ href => "/report/$t->{id}.$_->{num}", 'report';
                         txt_ ' >';
-                    } if !$_->{hidden} || can_edit t => $_;
-                    if($_->{hidden}) {
-                        i_ class => 'deleted', 'Post deleted.';
+                    } if !defined $_->{hidden} || can_edit t => $_;
+                    if(defined $_->{hidden}) {
+                        i_ class => 'deleted', sub {
+                            txt_ 'Post deleted';
+                            lit_ length $_->{hidden} ? ': '.bb_format $_->{hidden}, inline => 1 : '.';
+                        };
                     } else {
                         lit_ bb_format $_->{msg};
                         i_ class => 'lastmod', 'Last modified on '.fmtdate($_->{edited}, 'full') if $_->{edited};

lib/VNWeb/Discussions/UPosts.pm
@@ -41,16 +41,16 @@ TUWF::get qr{/$RE{uid}/posts}, sub {
     my $page = tuwf->validate(get => p => { upage => 1 })->data;
 
     my $sql = sql '(
-        SELECT tp.tid, tp.num, tp.msg, t.title, tp.date, t.hidden OR tp.hidden
+        SELECT tp.tid, tp.num, tp.msg, t.title, tp.date, t.hidden OR tp.hidden IS NOT NULL
           FROM threads_posts tp
           JOIN threads t ON t.id = tp.tid
-         WHERE tp.uid =', \$u->{id}, 'AND NOT t.private', auth->permBoardmod ? () : 'AND NOT t.hidden AND NOT tp.hidden', '
+         WHERE tp.uid =', \$u->{id}, 'AND NOT t.private', auth->permBoardmod ? () : 'AND NOT t.hidden AND tp.hidden IS NULL', '
        UNION ALL
-        SELECT rp.id, rp.num, rp.msg, v.title, rp.date, rp.hidden
+        SELECT rp.id, rp.num, rp.msg, v.title, rp.date, rp.hidden IS NOT NULL
           FROM reviews_posts rp
           JOIN reviews r ON r.id = rp.id
           JOIN vn v ON v.id = r.vid
-         WHERE rp.uid =', \$u->{id}, auth->permBoardmod ? () : 'AND NOT rp.hidden', '
+         WHERE rp.uid =', \$u->{id}, auth->permBoardmod ? () : 'AND rp.hidden IS NULL', '
        ) p(id,num,msg,title,date,hidden)';
 
     my $count = tuwf->dbVali('SELECT count(*) FROM', $sql);

lib/VNWeb/Misc/Feeds.pm
@@ -69,7 +69,7 @@ TUWF::get qr{/feeds/posts.atom}, sub {
         FROM threads_posts tp
         JOIN threads t ON t.id = tp.tid
         LEFT JOIN users u ON u.id = tp.uid
-       WHERE NOT tp.hidden AND NOT t.hidden AND NOT t.private
+       WHERE tp.hidden IS NULL AND NOT t.hidden AND NOT t.private
        ORDER BY tp.date DESC
        LIMIT ', \25
     );

lib/VNWeb/Misc/HomePage.pm
@@ -127,7 +127,7 @@ sub recent_vn_posts_ {
               JOIN reviews_posts wp ON wp.id = w.id AND wp.num = w.c_lastnum
               JOIN vn v ON v.id = w.vid
               LEFT JOIN users u ON wp.uid = u.id
-             WHERE NOT w.c_flagged AND NOT wp.hidden
+             WHERE NOT w.c_flagged AND wp.hidden IS NULL
              ORDER BY wp.date DESC LIMIT 10
         ) SELECT x.id, x.num, x.title,', sql_totime('x.date'), 'AS date, ', sql_user(), '
             FROM (SELECT * FROM tposts UNION ALL SELECT * FROM wposts) x

commit c119e60e3ddea4c86cabf8e0866b8a0b7f68ad26

Validation: Fix authorization of thread edits by creator
Broken since 8c1fcdb94b46373c4e9c8d533e645edf6aba6f7a (!?)

lib/VNWeb/Validation.pm
@@ -243,7 +243,9 @@ sub can_edit {
         } else {
             die "Can't do authorization test when hidden/date/user_id fields aren't present"
                 if !exists $entry->{hidden} || !exists $entry->{date} || !exists $entry->{user_id};
-            return auth && $entry->{user_id} eq auth->uid && !defined $entry->{hidden} && $entry->{date} > time-config->{board_edit_time};
+            # beware: for threads the 'hidden' field is a non-undef boolean flag, for posts it is a possibly-undef text field.
+            my $hidden = $entry->{id} =~ /^t/ && $entry->{num} == 1 ? $entry->{hidden} : defined $entry->{hidden};
+            return auth && $entry->{user_id} eq auth->uid && !$hidden && $entry->{date} > time-config->{board_edit_time};
         }
     }
 