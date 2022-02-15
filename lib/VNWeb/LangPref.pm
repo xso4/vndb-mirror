cec7f09f7d4b43627ed501e97200099f15d122c8
Yorhel <git@yorhel.nl>
2022-02-15 11:54:55 +0100

    Ignore language preferences in BBCode::bb_subst_links()
    "Fixes" https://vndb.org/t17804.88. It may be nicer to honor the
    preferences of the user *viewing* the rendered BBCode, but I don't
    really want to go there.
    
    This is a horrible, horrible hack.

--- lib/VNWeb/LangPref.pm
+++ lib/VNWeb/LangPref.pm
@@ -30,17 +30,18 @@ our $DEFAULT_TITLE_LANGS    = [{ lang => undef, latin => 1, official => 1 }];
 our $DEFAULT_ALTTITLE_LANGS = [{ lang => undef, latin => 0, official => 1 }];
 
 
-my $current = '';
+my $DEFAULT_SESSION = langpref_fmt($DEFAULT_TITLE_LANGS).langpref_fmt($DEFAULT_ALTTITLE_LANGS);
+my $CURRENT_SESSION = $DEFAULT_SESSION;
 
-TUWF::hook db_connect => sub { $current = '' };
+TUWF::hook db_connect => sub { $CURRENT_SESSION = $DEFAULT_SESSION };
 
 TUWF::hook before => sub {
     my $titles    = langpref_parse(auth->pref('title_langs'))    // $DEFAULT_TITLE_LANGS;
     my $alttitles = langpref_parse(auth->pref('alttitle_langs')) // $DEFAULT_ALTTITLE_LANGS;
 
     my $new = langpref_fmt($titles).langpref_fmt($alttitles);
-    return if $new eq $current;
-    $current = $new;
+    return if $new eq $CURRENT_SESSION;
+    $CURRENT_SESSION = $new;
 
     sub id { ($_[0]{official}?'o':'u').($_[0]{lang}//'') }
 
@@ -64,5 +65,21 @@ TUWF::hook before => sub {
         SELECT v.*, $title AS title, $alttitle AS alttitle
           FROM vn v", @joins);
 };
+
+
+# Run the given subroutine with the default language preferences, by
+# temporarily disabling any user preferences in the current database session.
+# (This function is a hack)
+sub run_with_defaults {
+    my($f) = @_;
+    return $f->() if $CURRENT_SESSION eq $DEFAULT_SESSION;
+    tuwf->dbExeci('SET search_path TO public,pg_temp');
+    my $r;
+    my $e = eval { $r = $f->(); 1 };
+    my $s = $@;
+    tuwf->dbExeci('SET search_path TO public');
+    die $s if !$e;
+    $r;
+}
 
 1;
