a4cc01a4d2246e5e8ce19ee5893aa6072f1eb37c
Yorhel <git@yorhel.nl>
2022-05-24 10:02:11 +0200

    JS: Fix retaining single-character search queries on tab switch
    Fixes https://vndb.org/t2520.659

--- elm/searchtabs.js
+++ elm/searchtabs.js
@@ -1,7 +1,7 @@
 document.querySelectorAll('#searchtabs a').forEach(function(l) {
     l.onclick = function() {
         var str = document.getElementById('q').value;
-        if(str.length > 1) {
+        if(str.length > 0) {
             if(this.href.indexOf('/g') >= 0 || this.href.indexOf('/i') >= 0)
                 this.href += '/list';
             this.href += '?q=' + encodeURIComponent(str);
