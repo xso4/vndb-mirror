0390c5d3fa7c1cb8d2b679f4ec7deb99ab48748c
Yorhel <git@yorhel.nl>
2022-05-30 07:18:57 +0200

    Chars::VNTab: Display a box even if all characters are hidden
    Otherwise you can't switch spoiler settings.
    
    Fixes https://vndb.org/t2520.665

--- lib/VNWeb/Chars/VNTab.pm
+++ lib/VNWeb/Chars/VNTab.pm
@@ -19,27 +19,34 @@ sub chars_ {
     $chars = [ grep +grep($_->{spoil} <= $view->{spoilers}, $_->{vns}->@*), @$chars ];
     my $has_sex = grep !$_->{hidden} && $_->{spoil} <= $view->{spoilers} && $_->{sexual}, map $_->{traits}->@*, @$chars;
 
+    my sub opts_ {
+        p_ class => 'mainopts', sub {
+            if($max_spoil) {
+                a_ mkclass(checked => $view->{spoilers} == 0), href => '?view='.viewset(spoilers=>0,traits_sexual=>$view->{traits_sexual}).'#chars', 'Hide spoilers';
+                a_ mkclass(checked => $view->{spoilers} == 1), href => '?view='.viewset(spoilers=>1,traits_sexual=>$view->{traits_sexual}).'#chars', 'Show minor spoilers';
+                a_ mkclass(standout =>$view->{spoilers} == 2), href => '?view='.viewset(spoilers=>2,traits_sexual=>$view->{traits_sexual}).'#chars', 'Spoil me!' if $max_spoil == 2;
+            }
+            b_ class => 'grayedout', ' | ' if $has_sex && $max_spoil;
+            a_ mkclass(checked => $view->{traits_sexual}), href => '?view='.viewset(spoilers=>$view->{spoilers},traits_sexual=>!$view->{traits_sexual}).'#chars', 'Show sexual traits' if $has_sex;
+        };
+    }
+
     my %done;
     my $first = 0;
     for my $r (keys %CHAR_ROLE) {
         my @c = grep grep($_->{role} eq $r, $_->{vns}->@*) && !$done{$_->{id}}++, @$chars;
         next if !@c;
         div_ class => 'mainbox', sub {
-
-            p_ class => 'mainopts', sub {
-                if($max_spoil) {
-                    a_ mkclass(checked => $view->{spoilers} == 0), href => '?view='.viewset(spoilers=>0,traits_sexual=>$view->{traits_sexual}).'#chars', 'Hide spoilers';
-                    a_ mkclass(checked => $view->{spoilers} == 1), href => '?view='.viewset(spoilers=>1,traits_sexual=>$view->{traits_sexual}).'#chars', 'Show minor spoilers';
-                    a_ mkclass(standout =>$view->{spoilers} == 2), href => '?view='.viewset(spoilers=>2,traits_sexual=>$view->{traits_sexual}).'#chars', 'Spoil me!' if $max_spoil == 2;
-                }
-                b_ class => 'grayedout', ' | ' if $has_sex && $max_spoil;
-                a_ mkclass(checked => $view->{traits_sexual}), href => '?view='.viewset(spoilers=>$view->{spoilers},traits_sexual=>!$view->{traits_sexual}).'#chars', 'Show sexual traits' if $has_sex;
-            } if !$first++;
-
+            opts_ if !$first++;
             h1_ $CHAR_ROLE{$r}{ @c > 1 ? 'plural' : 'txt' };
             VNWeb::Chars::Page::chartable_($_, 1, $_ != $c[0], 1) for @c;
         }
     }
+
+    div_ class => 'mainbox', sub {
+        opts_;
+        h1_ '(Characters hidden by spoiler settings)';
+    } if !$first;
 }
 
 
