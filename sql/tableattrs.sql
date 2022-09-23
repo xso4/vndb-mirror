af1da03a19e056ad864640ad14e9b26a0fcfc19f
Yorhel <git@yorhel.nl>
2022-09-23 13:31:36 +0200

    sql: Fix definition of foreign key constraint

--- sql/tableattrs.sql
+++ sql/tableattrs.sql
@@ -143,7 +143,7 @@ ALTER TABLE ulist_vns_labels         ADD CONSTRAINT ulist_vns_labels_uid_lbl_fke
 ALTER TABLE ulist_vns_labels         ADD CONSTRAINT ulist_vns_labels_uid_vid_fkey      FOREIGN KEY (uid,vid)   REFERENCES ulist_vns     (uid,vid) ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE users_prefs              ADD CONSTRAINT users_prefs_id_fkey                FOREIGN KEY (id)        REFERENCES users         (id) ON DELETE CASCADE;
 ALTER TABLE users_prefs_tags         ADD CONSTRAINT users_prefs_tags_id_fkey           FOREIGN KEY (id)        REFERENCES users         (id) ON DELETE CASCADE;
-ALTER TABLE users_prefs_tags         ADD CONSTRAINT users_prefs_tags_tag_fkey          FOREIGN KEY (tag)       REFERENCES tags          (id) ON DELETE CASCADE;
+ALTER TABLE users_prefs_tags         ADD CONSTRAINT users_prefs_tags_tid_fkey          FOREIGN KEY (tid)       REFERENCES tags          (id) ON DELETE CASCADE;
 ALTER TABLE users_prefs_traits       ADD CONSTRAINT users_prefs_traits_id_fkey         FOREIGN KEY (id)        REFERENCES users         (id) ON DELETE CASCADE;
 ALTER TABLE users_prefs_traits       ADD CONSTRAINT users_prefs_traits_tid_fkey        FOREIGN KEY (tid)       REFERENCES traits        (id) ON DELETE CASCADE;
 ALTER TABLE users_shadow             ADD CONSTRAINT users_shadow_id_fkey               FOREIGN KEY (id)        REFERENCES users         (id) ON DELETE CASCADE;
