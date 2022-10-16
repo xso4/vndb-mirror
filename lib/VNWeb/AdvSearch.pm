xso4/vndb-mirror

commit 1dfa4514e162f0e36baf81affda9c94e9499e918

API2: Initial experiments
It's looking pretty powerful and functional already.

lib/VNWeb/AdvSearch.pm
@@ -310,6 +310,7 @@ sub f {
 my @TYPE; # stack of query types, $TYPE[0] is the top-level query, $TYPE[$#TYPE] the query currently being processed.
 
 
+f v => 80 => 'id',        { vndbid => 'v' }, '=' => sub { sql 'v.id = ', \$_ };
 f v =>  2 => 'lang',      { enum => \%LANGUAGE }, '=' => sub { sql 'v.c_languages && ARRAY', \$_, '::language[]' };
 f v =>  3 => 'olang',     { enum => \%LANGUAGE }, '=' => sub { sql 'v.olang =', \$_ };
 f v =>  4 => 'platform',  { enum => \%PLATFORM }, '=' => sub { sql 'v.c_platforms && ARRAY', \$_, '::platform[]' };
@@ -318,12 +319,12 @@ f v =>  5 => 'length',    { uint => 1, enum => \%VN_LENGTH },
 f v =>  7 => 'released',  { fuzzyrdate => 1 }, sql => sub { sql 'v.c_released', $_[0], \($_ == 1 ? strftime('%Y%m%d', gmtime) : $_) };
 f v =>  9 => 'popularity',{ uint => 1, range => [ 0,  100] }, sql => sub { sql 'v.c_popularity', $_[0], \($_*100) };
 f v => 10 => 'rating',    { uint => 1, range => [10,  100] }, sql => sub { sql 'v.c_rating', $_[0], \($_*10) };
-f v => 11 => 'vote-count',{ uint => 1, range => [ 0,1<<30] }, sql => sub { sql 'v.c_votecount', $_[0], \$_ };
-f v => 61 => 'has-description', { uint => 1, range => [1,1] }, '=' => sub { 'v."desc" <> \'\'' };
-f v => 62 => 'has-anime',       { uint => 1, range => [1,1] }, '=' => sub { 'EXISTS(SELECT 1 FROM vn_anime va WHERE va.id = v.id)' };
-f v => 63 => 'has-screenshot',  { uint => 1, range => [1,1] }, '=' => sub { 'EXISTS(SELECT 1 FROM vn_screenshots vs WHERE vs.id = v.id)' };
-f v => 64 => 'has-review',      { uint => 1, range => [1,1] }, '=' => sub { 'EXISTS(SELECT 1 FROM reviews r WHERE r.vid = v.id AND NOT r.c_flagged)' };
-f v => 65 => 'on-list',         { uint => 1, range => [1,1] }, '=' => sub { auth ? sql 'v.id IN(SELECT vid FROM ulist_vns WHERE uid =', \auth->uid, ')' : '1=0' };
+f v => 11 => 'votecount', { uint => 1, range => [ 0,1<<30] }, sql => sub { sql 'v.c_votecount', $_[0], \$_ };
+f v => 61 => 'has_description', { uint => 1, range => [1,1] }, '=' => sub { 'v."desc" <> \'\'' };
+f v => 62 => 'has_anime',       { uint => 1, range => [1,1] }, '=' => sub { 'EXISTS(SELECT 1 FROM vn_anime va WHERE va.id = v.id)' };
+f v => 63 => 'has_screenshot',  { uint => 1, range => [1,1] }, '=' => sub { 'EXISTS(SELECT 1 FROM vn_screenshots vs WHERE vs.id = v.id)' };
+f v => 64 => 'has_review',      { uint => 1, range => [1,1] }, '=' => sub { 'EXISTS(SELECT 1 FROM reviews r WHERE r.vid = v.id AND NOT r.c_flagged)' };
+f v => 65 => 'on_list',         { uint => 1, range => [1,1] }, '=' => sub { auth ? sql 'v.id IN(SELECT vid FROM ulist_vns WHERE uid =', \auth->uid, ')' : '1=0' };
 f v => 66 => 'devstatus', { uint => 1, enum => \%DEVSTATUS }, '=' => sub { 'v.devstatus =', \$_ };
 
 f v =>  8 => 'tag',      { type => 'any', func => \&_validate_tag }, compact => \&_compact_tag, sql_list => _sql_where_tag('tags_vn_inherit');
@@ -333,7 +334,7 @@ f v => 12 => 'label',    { type => 'any', func => \&_validate_label },
     compact => sub { [ ($_->[0] =~ s/^u//r)*1, $_->[1]*1 ] },
     sql_list => \&_sql_where_label, sql_list_grp => sub { $_->[1] == 0 ? undef : $_->[0] };
 
-f v => 13 => 'anime-id',  { id => 1 },
+f v => 13 => 'anime_id',  { id => 1 },
     sql_list => sub {
         my($neg, $all, $val) = @_;
         sql 'v.id', $neg ? 'NOT' : '', 'IN(SELECT id FROM vn_anime WHERE aid IN', $val, $all && @$val > 1 ? ('GROUP BY id HAVING COUNT(aid) =', \scalar @$val) : (), ')';
@@ -546,6 +547,10 @@ sub _validate_adv {
         my($v,$i) = ($_[0],0);
         return { msg => 'Invalid compact encoded form', character_index => $i } if !($_[0] = _dec_query($v, \$i));
         return { msg => 'Trailing garbage' } if $i != length $v;
+    }
+    if(ref $_[0] eq 'ARRAY' && $_[0]->@* == 0) {
+        $_[0] = bless {type=>$t}, __PACKAGE__;
+        return 1;
     }
     my $v = _validate($t, @_);
     $_[0] = bless { type => $t, query => $_[0] }, __PACKAGE__ if $v;

Makefile
@@ -5,6 +5,7 @@
 #   Create static assets for production. Requires the following additional dependencies:
 #   - uglifyjs
 #   - zopfli
+#   - pandoc
 #
 # chmod
 #   For when the http process is run from a different user than the files are
@@ -35,6 +36,7 @@ ALL_CLEAN=\
 	$(shell ls css/skins/*.sass | sed -e 's/css\/skins\/\(.\+\)\.sass/static\/g\/\1.css/g')
 
 PROD=\
+	static/g/api-kana.html \
 	static/g/plain.min.js static/g/plain.min.js.gz \
 	static/g/elm.min.js static/g/elm.min.js.gz \
 	static/g/icons.opt.png \
@@ -153,6 +155,11 @@ static/g/elm.min.js: ${ELM_FILES} elm/Gen/.generated | static/g
 		'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe'\
 		| uglifyjs --mangle --comments all -o $@~
 	mv $@~ $@
+
+
+
+static/g/api-kana.html: data/api-kana.md
+	pandoc "$<" -st html5 --toc -o "$@"
 
 
 

lib/VNWeb/API.pm
@@ -1,0 +1,316 @@
+package VNWeb::API::Index;
+
+use VNWeb::Prelude;
+
+TUWF::get qr{/api/kana}, sub {
+    state $data = do {
+        open my $F, '<', config->{root}.'/static/g/api-kana.html' or die $!;
+        local $/=undef;
+        my $url = tuwf->reqURI;
+        <$F> =~ s/%endpoint%/$url/rg;
+    };
+    tuwf->resHeader('Content-Type' => "text/html; charset=UTF-8");
+    tuwf->resBinary($data);
+};
+
+
+TUWF::options qr{/api/kana.*}, sub {
+    tuwf->resStatus(204);
+    tuwf->resHeader('Access-Control-Allow-Origin', '*');
+    tuwf->resHeader('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+    tuwf->resHeader('Access-Control-Allow-Headers', 'Content-Type');
+    tuwf->resHeader('Access-Control-Max-Age', 86400);
+};
+
+
+# TODO: accounting & logging
+sub api_get {
+    my($path, $schema, $sub) = @_;
+    my $s = tuwf->compile({ type => 'hash', keys => $schema });
+    TUWF::get qr{/api/kana\Q$path}, sub {
+        my $res = $sub->();
+        $s->analyze->coerce_for_json($res, unknown => 'reject');
+        tuwf->resJSON($res);
+        tuwf->resHeader('Access-Control-Allow-Origin', '*') if tuwf->reqHeader('Origin');
+    };
+}
+
+
+# %opt:
+#   filters => AdvSearch query type
+#   sql => sub { sql 'SELECT id', $_[0], 'FROM x', $_[1], 'WHERE', $_[2] },
+#       Main query to fetch items,
+#           $_[0] is the list of fields to fetch (including a preceding comma)
+#           $_[1] is a list of JOIN clauses
+#           $_[2] the filters for in the WHERE clause
+#       'ORDER BY' and 'LIMIT' clauses are appended to the returned query.
+#       Query must always return a column named 'id'.
+#   joins => {
+#       $name => $sql,
+#       # List of optional JOIN clauses that can be referenced by fields.
+#       # These should always be 1-to-1 joins, i.e. no filtering or expansion may take place.
+#   },
+#   fields => {
+#       $name => { %field_definition },
+#   },
+#   sort => [
+#       $name => $sql,
+#           SQL may include '?o' and '!o' placeholders, see TableOpts.pm.
+#       First sort option listed is the default.
+#   ],
+#
+# %field_definition for simple fields:
+#   select => 'SQL string',
+#   col    => 'name',  # Name of the column returned by 'SQL string',
+#                      # if it does not match the $name of the field.
+#   join   => 'name',  # This field requires a JOIN clause, refers to the 'joins' list above.
+#   proc   => sub {},  # Subroutine to do some formatting/processing of the value.
+#                      #   $_[0] is the value as returned from the DB, should be modified in-place.
+#
+# %field_definition for nested 1-to-1 objects:
+#   fields => {},    # Same as the parents' "fields" definitions.
+#                    # Can only be used to nest simple fields at a single level.
+#   nullif => 'SQL string',
+#                    # The entire object itself is set to null if this SQL value is true.
+#                    # The SQL string must return a column named "${fieldname}_nullif}".
+#
+# %field_definition for nested 1-to-many objects:
+#   enrich => sub { sql('SELECT id', $_[0], 'FROM x', $_[1], 'WHERE id IN') },
+#             # Subroutine that returns the $sql argument to enrich()
+#             #    $_[0] is the list of fields to fetch
+#             #    $_[1] is a list of JOIN clauses
+#   key    => 'id',  # $key argument to enrich()
+#   col    => 'id',  # $merge_col argument to enrich()
+#   joins  => {},    # Nested join definitions
+#   fields => {},    # Nested field definitions
+#
+# TODO: enrich_merge?
+sub api_query {
+    my($path, %opt) = @_;
+
+    my %sort = $opt{sort}->@*;
+    my $req_schema = tuwf->compile({ type => 'hash', keys => {
+        filters => { required => 0, advsearch => $opt{filters} },
+        fields => { required => 0, default => $opt{fields}, func => sub { parse_fields($opt{fields}, $_[0]) } },
+        sort => { required => 0, default => $opt{sort}[0], enum => [ keys %sort ] },
+        reverse => { required => 0, default => 0, jsonbool => 1 },
+        results => { required => 0, default => 10, uint => 1, range => [1,100] },
+        page => { required => 0, default => 1, uint => 1, range => [1,1e6] },
+        count => { required => 0, default => 0, jsonbool => 1 },
+        compact_filters => { required => 0, default => 0, jsonbool => 1 },
+        normalized_filters => { required => 0, default => 0, jsonbool => 1 },
+    }});
+
+    TUWF::post qr{/api/kana\Q$path}, sub {
+        # First make sure to reset any existing 'enabled' flags from a previous call,
+        # so these can be filled again with parse_fields() during validation.
+        # Writing directly to our config structure is a bit ugly, but at least it's simple and efficient.
+        (sub {
+            $_->{enabled} && (do{$_->{enabled} = 0} || $_->{fields}) && __SUB__->($_->{fields}) for values $_[0]->%*;
+        })->($opt{fields});
+
+        my $req = tuwf->validate(json => $req_schema);
+        if (!$req) {
+            tuwf->resStatus(400);
+            tuwf->resHeader('Content-type', 'text');
+            lit_ "Invalid query.\n";
+            eval { $req->data }; warn $@;
+            return;
+        }
+        $req = $req->data;
+
+        my $sort = $sort{$req->{sort}};
+        my $order = $req->{reverse} ? 'DESC' : 'ASC';
+        my $opposite_order = $req->{reverse} ? 'ASC' : 'DESC';
+        $sort = $sort =~ /[?!]o/ ? ($sort =~ s/\?o/$order/rg =~ s/!o/$opposite_order/rg) : "$sort $order";
+
+        my($select, $joins) = prepare_fields($req->{fields}, $opt{joins});
+
+        # TODO: Handle query timeouts
+        my($results, $more) = tuwf->dbPagei($req, $opt{sql}->($select, $joins, $req->{filters}->sql_where), 'ORDER BY', $sort);
+        my $count = $req->{count} && tuwf->dbVali('SELECT count(*) FROM (', $opt{sql}->('', '', $req->{filters}->sql_where), ') x');
+
+        proc_results($req->{fields}, $results);
+
+        tuwf->resJSON({
+            results => $results,
+            more => $more?\1:\0,
+            $req->{count} ? (count => $count) : (),
+            $req->{compact_filters} ? (compact_filters => $req->{filters}->query_encode) : (),
+            $req->{normalized_filters} ? (normalized_filters => $req->{filters}->json) : (),
+        });
+        tuwf->resHeader('Access-Control-Allow-Origin', '*') if tuwf->reqHeader('Origin');
+    };
+}
+
+
+sub parse_fields {
+    my @tokens = split /\s*([,.{}])\s*/, $_[1];
+    $_[1] = $_[0];
+    return (sub {
+        my($lvl, $f) = @_;
+        my $nf = $f;
+        while(defined (my $t = shift @tokens)) {
+            next if !length $t;
+            if($t eq '}') {
+                return { msg => "Invalid '}' after non-terminal field" } if $nf;
+                return $lvl > 0 ? 1 : { msg => "Unmatched '}'" } ;
+            } elsif($t eq '{') {
+                return { msg => "Invalid '{' after terminal field" } if !$nf;
+                my $r = __SUB__->($lvl+1, $nf);
+                return $r if ref $r;
+                $nf = undef;
+            } elsif($t eq ',') {
+                return { msg => 'Invalid comma after non-terminal field' } if $nf;
+                $nf = $f;
+            } else {
+                return { msg => 'Invalid attempt to enter terminal field' } if !$nf;
+                if($t eq '.') {
+                    $t = shift(@tokens) // return { msg => "Expected name after '.'" };
+                }
+                my $d = $nf->{$t} // return { msg => "Field not found", name => $t };
+                $d->{enabled} = 1;
+                $nf = $d->{fields};
+            }
+        }
+        return { msg => "Requested non-terminal field" } if $nf;
+        return $lvl > 0 ? { msg => "Unmatched '{'" } : 1;
+    })->(0, $_[1]);
+}
+
+
+sub prepare_fields {
+    my($fields, $joins) = @_;
+    my(@select, %join);
+    (sub {
+        for my $f (keys $_[0]->%*) {
+            my $d = $_[0]{$f};
+            next if !$d->{enabled};
+            $join{$d->{join}} = 1 if $d->{join};
+            push @select, $d->{select} if $d->{select};
+            push @select, $d->{nullif} if $d->{nullif};
+            __SUB__->($d->{fields}) if $d->{fields} && !$d->{enrich};
+        }
+    })->($fields);
+    return (
+        join('', map ",$_", @select),
+        join(' ', map $joins->{$_}, keys %join),
+    );
+}
+
+
+sub proc_field {
+    my($n, $d, $obj, $out) = @_;
+    $out->{$n} = delete $obj->{$d->{col}} if $d->{col};
+    $d->{proc}->($out->{$n}) if $d->{proc};
+}
+
+sub proc_results {
+    my($fields, $results) = @_;
+    for my $f (keys %$fields) {
+        my $d = $fields->{$f};
+        next if !$d->{enabled};
+
+        # nested 1-to-many objects
+        if($d->{enrich}) {
+            my($select, $join) = prepare_fields($d->{fields}, $d->{joins});
+            enrich $f, $d->{key}, $d->{col}, $d->{enrich}->($select, $join), $results;
+            proc_results($d->{fields}, [map $_->{$f}->@*, @$results]);
+
+        # nested 1-to-1 objects
+        } elsif($d->{fields}) {
+            for my $o (@$results) {
+                if($d->{nullif} && $o->{"${f}_nullif"}) {
+                    $o->{$f} = undef;
+                } else {
+                    $o->{$f} = {};
+                    proc_field($_, $d->{fields}{$_}, $o, $o->{$f})
+                        for grep $d->{fields}{$_}{enabled}, keys $d->{fields}->%*;
+                }
+            }
+
+        # simple fields
+        } else {
+            proc_field($f, $d, $_, $_) for @$results;
+        }
+    }
+}
+
+
+my @STATS = qw{traits producers tags chars staff vn releases};
+api_get '/stats', { map +($_, { uint => 1 }), @STATS }, sub {
+    +{ map +($_->{section}, $_->{count}),
+        tuwf->dbAlli('SELECT * FROM stats_cache WHERE section IN', \@STATS)->@* };
+};
+
+
+
+my @BOOL = (proc => sub { $_[0] = $_[0] ? \1 : \0 if defined $_[0] });
+my @INT = (proc => sub { $_[0] *= 1 if defined $_[0] });
+
+sub IMG {
+    my($main_col, $join_id, $join_prefix) = @_;
+    return (
+        id        => { select => "$main_col AS image_id", col => 'image_id' },
+        url       => { select => "$main_col AS image_url", col => 'image_url', proc => sub { $_[0] = imgurl $_[0] } },
+        dims      => { join => $join_id, col => 'image_dims', select => "ARRAY[${join_prefix}width, ${join_prefix}height] AS image_dims" },
+        sexual    => { join => $join_id, select => "${join_prefix}c_sexual_avg", col => 'c_sexual_avg' },
+        violence  => { join => $join_id, select => "${join_prefix}c_violence_avg", col => 'c_violence_avg' },
+        votecount => { join => $join_id, select => "${join_prefix}c_votecount AS image_votecount", col => 'image_votecount' },
+    );
+}
+
+api_query '/vn',
+    filters => 'v',
+    sql => sub { sql 'SELECT v.id', $_[0], 'FROM vnt v', $_[1], 'WHERE NOT v.hidden AND (', $_[2], ')' },
+    joins => {
+        image => 'LEFT JOIN images i ON i.id = v.image',
+    },
+    fields => {
+        title => { select => 'v.title' },
+        alttitle => { select => 'v.alttitle' },
+        titles => {
+            enrich => sub { sql 'SELECT vt.id', $_[0], 'FROM vn_titles vt', $_[1], 'WHERE vt.id IN' },
+            key => 'id', col => 'id',
+            joins => {
+                main => 'JOIN vn v ON v.id = vt.id',
+            },
+            fields => {
+                lang  => { select => 'vt.lang' },
+                title => { select => 'vt.title' },
+                latin => { select => 'vt.latin' },
+                official => { select => 'vt.official', @BOOL },
+                main => { join => 'main', select => 'vt.lang = v.olang AS main', @BOOL },
+            },
+        },
+        olang => { select => 'v.olang' },
+        image => {
+            fields => { IMG 'v.image', 'image', 'i.' },
+            nullif => 'v.image IS NULL AS image_nullif',
+        },
+        screenshots => {
+            enrich => sub { sql 'SELECT vs.id AS vid', $_[0], 'FROM vn_screenshots vs', $_[1], 'WHERE vs.id IN' },
+            key => 'id', col => 'vid',
+            joins => {
+                image => 'JOIN images i ON i.id = vs.scr',
+            },
+            fields => {
+                IMG('vs.scr', 'image', 'i.'),
+                thumbnail => { select => "vs.scr AS thumbnail", col => 'thumbnail', proc => sub { $_[0] = imgurl $_[0], 1 } },
+                thumbnail_dims => { join => 'image', col => 'thumbnail_dims'
+                                  , select => "ARRAY[i.width, i.height] AS thumbnail_dims"
+                                  , proc => sub { @{$_[0]} = imgsize @{$_[0]}, config->{scr_size}->@* } },
+                # TODO: release info
+            },
+        },
+    },
+    sort => [
+        id => 'v.id',
+        title => 'v.sorttitle ?o, v.id',
+        released => 'v.c_released ?o, v.id',
+        popularity => 'v.c_pop_rank !o NULLS LAST, v.id',
+        rating => 'v.c_rat_rank !o NULLS LAST, v.id',
+        votecount => 'v.c_votecount ?o, v.id',
+    ];
+
+1;

lib/VNWeb/Auth.pm
@@ -37,7 +37,8 @@ use VNWeb::DB;
 our @EXPORT = ('auth');
 
 sub auth {
-    tuwf->req->{auth} ||= do {
+    # API requests do not (currently) use this Auth system.
+    tuwf->req->{auth} ||= tuwf->reqPath =~ qr{^/api/} ? __PACKAGE__->new() : do {
         my $cookie = tuwf->reqCookie('auth')||'';
         my($uid, $token_e) = $cookie =~ /^([a-fA-F0-9]{40})\.?u?(\d+)$/ ? ('u'.$2, sha1_hex pack 'H*', $1) : (0, '');
 

lib/VNWeb/Filters.pm
@@ -161,8 +161,8 @@ sub filter_vn_adv {
     defined $fil->{date_after}  ? [ 'released', '>=', $fil->{date_after} ] : (),
     defined $fil->{released}    ? [ 'released', $fil->{released} ? '<=' : '>', 1 ] : (),
     defined $fil->{length}      ? [ 'or', map [ 'length', '=', $_ ], $fil->{length}->@* ] : (),
-    defined $fil->{hasani}      ? [ 'has-anime', $fil->{hasani} ? '=' : '!=', 1 ] : (),
-    defined $fil->{hasshot}     ? [ 'has-screenshot', $fil->{hasshot} ? '=' : '!=', 1 ] : (),
+    defined $fil->{hasani}      ? [ 'has_anime', $fil->{hasani} ? '=' : '!=', 1 ] : (),
+    defined $fil->{hasshot}     ? [ 'has_screenshot', $fil->{hasshot} ? '=' : '!=', 1 ] : (),
     defined $fil->{tag_inc}     ? [ 'and', map [ 'tag', '=',  [ $_, $fil->{tagspoil}, 0 ] ], $fil->{tag_inc}->@* ] : (),
     defined $fil->{tag_exc}     ? [ 'and', map [ 'tag', '!=', [ $_, 2, 0 ] ], $fil->{tag_exc}->@* ] : (),
     defined $fil->{lang}        ? [ 'or', map [ 'lang',     '=', $_ ], $fil->{lang}->@* ] : (),

lib/VNWeb/Validation.pm
@@ -43,7 +43,7 @@ TUWF::set custom_validations => {
     language    => { enum => \%LANGUAGE },
     gtin        => { required => 0, default => 0, func => sub { $_[0] = 0 if !length $_[0]; $_[0] eq 0 || gtintype($_[0]) } },
     rdate       => { uint => 1, func => \&_validate_rdate },
-    fuzzyrdate  => { func => \&_validate_fuzzyrdate },
+    fuzzyrdate  => { required => 0, default => 0, func => \&_validate_fuzzyrdate },
     # An array that may be either missing (returns undef), a single scalar (returns single-element array) or a proper array
     undefarray  => sub { +{ required => 0, default => undef, type => 'array', scalar => 1, values => $_[0] } },
     # Accepts a user-entered vote string (or '-' or empty) and converts that into a DB vote number (or undef) - opposite of fmtvote()
@@ -81,9 +81,9 @@ sub _validate_rdate {
 
 
 sub _validate_fuzzyrdate {
-    $_[0] = 0 if $_[0] =~ /^unknown$/;
-    $_[0] = 1 if $_[0] =~ /^today$/;
-    $_[0] = 99999999 if $_[0] =~ /^tba$/;
+    $_[0] = 0 if $_[0] =~ /^unknown$/i;
+    $_[0] = 1 if $_[0] =~ /^today$/i;
+    $_[0] = 99999999 if $_[0] =~ /^tba$/i;
     $_[0] = "${1}9999" if $_[0] =~ /^([0-9]{4})$/;
     $_[0] = "${1}${2}99" if $_[0] =~ /^([0-9]{4})-([0-9]{2})$/;
     $_[0] = "${1}${2}$3" if $_[0] =~ /^([0-9]{4})-([0-9]{2})-([0-9]{2})$/;

util/vndb.pl
@@ -35,6 +35,8 @@ tuwf->{elmgen} = $ARGV[0] && $ARGV[0] eq 'elmgen';
 
 
 TUWF::hook before => sub {
+    return if tuwf->reqPath =~ qr{^/api/};
+
     # Serve static files from www/
     if(tuwf->resFile("$ROOT/www", tuwf->reqPath)) {
         tuwf->resHeader('Cache-Control' => 'max-age=86400');
@@ -60,6 +62,11 @@ TUWF::hook before => sub {
 
 TUWF::set error_404_handler => sub {
     tuwf->resStatus(404);
+    if(tuwf->reqPath =~ /^\/api\//) {
+        tuwf->resHeader('Content-Type', 'text/plain');
+        lit_ "Not found.\n";
+        return;
+    }
     VNWeb::HTML::framework_ title => 'Page Not Found', noindex => 1, sub {
         div_ class => 'mainbox', sub {
             h1_ 'Page not found';
@@ -124,6 +131,7 @@ TUWF::set import_modules => 0;
 TUWF::load_recursive('VNWeb');
 
 TUWF::hook after => sub {
+    return if tuwf->reqPath =~ qr{^/api/};
     return if rand() > config->{trace_log} || !tuwf->req->{trace_start};
     my $sqlt = List::Util::sum(map $_->[2], tuwf->{_TUWF}{DB}{queries}->@*);
     my %elm = (

commit 9fbc8d6accd8f8b9bb98e4a58c1673bd1416fe67

AdvSearch: Fix fetching title of anime_id filter
Fixes https://vndb.org/t2520.702
Broken in 1dfa4514e162f0e36baf81affda9c94e9499e918

lib/VNWeb/AdvSearch.pm
@@ -755,7 +755,7 @@ sub _extract_ids {
     } else {
         my $f = $FIELDS{$t}{$q->[0]};
         $ids->{$q->[2]} = 1 if $f->{vndbid};
-        $ids->{"anime$q->[2]"} = 1 if $q->[0] eq 'anime-id';
+        $ids->{"anime$q->[2]"} = 1 if $q->[0] eq 'anime_id';
         $ids->{$q->[2][0]} = 1 if ref $f->{value} && ref $q->[2] eq 'ARRAY'; # Ugly heuristic, may have false positives
         _extract_ids($f->{value}, $q->[2], $ids) if !ref $f->{value};
     }