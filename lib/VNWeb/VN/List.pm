xso4/vndb-mirror

commit 138944ced5a32236afa479c418a33dc7ae8cf9b5

AdvSearch: Add option to exclude lie tags
https://vndb.org/t18886.24

The compact form encoding is a big ugly mess, I wouldn't be surprised if
there's bugs.

lib/VNWeb/VN/List.pm
@@ -234,7 +234,7 @@ TUWF::get qr{/v(?:/(?<char>all|[a-z0]))?}, sub {
         q => { onerror => undef },
         sq=> { onerror => undef },
         p => { upage => 1 },
-        f => { advsearch_err => 'v' },
+        f => { advsearch => 'v' },
         s => { tableopts => $TABLEOPTS },
         ch=> { onerror => [], type => 'array', scalar => 1, values => { onerror => undef, enum => ['0', 'a'..'z'] } },
         fil  => { required => 0 },

elm/AdvSearch/Fields.elm
@@ -431,13 +431,15 @@ fields =
   , f V "Language"           1  FMLang       (AS.langInit AS.LangVN)  (AS.langFromQuery AS.LangVN)
   , f V "Original language"  2  FMLang       (AS.langInit AS.LangVNO) (AS.langFromQuery AS.LangVNO)
   , f V "Platform"           3  FMVPlatform   AS.init                 AS.platformFromQuery
-  , f V "Tags"               4  FMTag         AG.init                 (AG.fromQuery -1 True)
-  , f V ""                  -4  FMTag         AG.init                 (AG.fromQuery 0 True)
-  , f V ""                  -4  FMTag         AG.init                 (AG.fromQuery 1 True)
-  , f V ""                  -4  FMTag         AG.init                 (AG.fromQuery 2 True)
-  , f V ""                  -4  FMTag         AG.init                 (AG.fromQuery 0 False)
-  , f V ""                  -4  FMTag         AG.init                 (AG.fromQuery 1 False)
-  , f V ""                  -4  FMTag         AG.init                 (AG.fromQuery 2 False)
+  , f V "Tags"               4  FMTag         AG.init                 (AG.fromQuery -1 True False)
+  , f V ""                  -4  FMTag         AG.init                 (AG.fromQuery 0 True False)
+  , f V ""                  -4  FMTag         AG.init                 (AG.fromQuery 1 True False)
+  , f V ""                  -4  FMTag         AG.init                 (AG.fromQuery 2 True False)
+  , f V ""                  -4  FMTag         AG.init                 (AG.fromQuery 2 True True )
+  , f V ""                  -4  FMTag         AG.init                 (AG.fromQuery 0 False False)
+  , f V ""                  -4  FMTag         AG.init                 (AG.fromQuery 1 False False)
+  , f V ""                  -4  FMTag         AG.init                 (AG.fromQuery 2 False False)
+  , f V ""                  -4  FMTag         AG.init                 (AG.fromQuery 2 False True )
   , f V "My Labels"          0  FMLabel       AS.init                 AS.labelFromQuery
   , l V "My List"            0 [(QInt 65 Eq 1, "On my list"),         (QInt 65 Ne 1, "Not on my list")]
   , f V "Length"             0  FMLength      AS.init                 AS.lengthFromQuery

elm/AdvSearch/Tags.elm
@@ -19,6 +19,7 @@ type alias Model =
   , search  : A.Model GApi.ApiTagResult
   , spoiler : Int
   , inherit : Bool
+  , exclie  : Bool
   }
 
 type Msg
@@ -26,6 +27,7 @@ type Msg
   | Level (Int,Int) Int
   | Spoiler
   | Inherit Bool
+  | ExcLie Bool
   | Search (A.Msg GApi.ApiTagResult)
 
 
@@ -38,6 +40,7 @@ init dat =
         , search  = A.init ""
         , spoiler = dat.defaultSpoil
         , inherit = True
+        , exclie  = False
         }
       )
 
@@ -47,8 +50,9 @@ update dat msg model =
   case msg of
     Sel m -> (dat, { model | sel = S.update m model.sel }, Cmd.none)
     Level (t,ol) nl -> (dat, { model | sel = S.update (S.Sel (t,ol) False) model.sel |> S.update (S.Sel (t,nl) True) }, Cmd.none)
-    Spoiler -> (dat, { model | spoiler = if model.spoiler < 2 then model.spoiler + 1 else 0 }, Cmd.none)
+    Spoiler -> (dat, { model | spoiler = if model.spoiler < 2 then model.spoiler + 1 else 0, exclie = False }, Cmd.none)
     Inherit b -> (dat, { model | inherit = b }, Cmd.none)
+    ExcLie b  -> (dat, { model | exclie  = b }, Cmd.none)
     Search m ->
       let (nm, c, res) = A.update model.conf m model.search
       in case res of
@@ -61,13 +65,13 @@ update dat msg model =
 
 toQuery m = S.toQuery (\o (t,l) ->
   let id = if m.inherit then 8 else 14
-  in if m.spoiler == 0 && l == 0 then QInt id o t else QTuple id o t (l*3+m.spoiler)) m.sel
+  in if m.spoiler == 0 && not m.exclie && l == 0 then QInt id o t else QTuple id o t ((if m.exclie then 16*3 else 0) + l*3 + m.spoiler)) m.sel
 
-fromQuery spoil inherit dat q =
+fromQuery spoil inherit exclie dat q =
   let id = if inherit then 8 else 14
       f qr = case qr of
-              QInt x op t -> if id == x && spoil == 0 then Just (op, (t,0)) else Nothing
-              QTuple x op t v -> if id == x && modBy 3 v == spoil then Just (op, (t,v//3)) else Nothing
+              QInt x op t -> if id == x && spoil == 0 && not exclie then Just (op, (t,0)) else Nothing
+              QTuple x op t v -> if id == x && modBy 3 v == spoil && exclie == ((v // (16*3)) == 1) then Just (op, (t, modBy 16 (v//3))) else Nothing
               _ -> Nothing
   in
   S.fromQuery f dat q |> Maybe.map (\(ndat,sel) ->
@@ -77,6 +81,7 @@ fromQuery spoil inherit dat q =
       , search  = A.init ""
       , spoiler = spoil
       , inherit = inherit
+      , exclie  = exclie
       }
     ))
 
@@ -100,7 +105,11 @@ view dat model =
           [ text <| if model.spoiler == 0 then "no spoilers" else if model.spoiler == 1 then "minor spoilers" else "major spoilers" ]
         , linkRadio model.sel.neg (Sel << S.Neg) [ text "invert" ]
         ]
-      , div [ class "opts" ] [ span [] [], linkRadio model.inherit Inherit [ text "child tags" ] ]
+      , div [ class "opts" ]
+        [ if model.spoiler < 2 then span [] [] else
+          linkRadio model.exclie ExcLie [ text "exclude lies" ]
+        , linkRadio model.inherit Inherit [ text "child tags" ]
+        ]
       ]
     , ul [] <| List.map (\(t,l) ->
         li [ style "overflow" "hidden", style "text-overflow" "ellipsis" ]

lib/VNWeb/AdvSearch.pm
@@ -484,7 +484,12 @@ f i =>  2 => 'id',        { vndbid => 'i' }, sql => sub { sql 't.id', $_[0], \$_
 f i => 80 => 'search',    {}, '=' => sub { sql 't.c_search LIKE ALL (search_query(', \$_, '))' };
 
 
-# Accepts either $tag or [$tag, int($minlevel*5)*3+$maxspoil] (for compact form) or [$tag, $maxspoil, $minlevel]. Normalizes to the latter.
+# Accepts either:
+# - $tag
+# - [$tag, $exclude_lies*16*3 + int($minlevel*5)*3 + $maxspoil] (compact form)
+# - [$tag, $maxspoil, $minlevel]
+# - [$tag, $maxspoil, $minlevel, $exclude_lies]
+# Normalizes to the latter two.
 sub _validate_tag {
     $_[0] = [$_[0],0,0] if ref $_[0] ne 'ARRAY'; # just a tag id
     my $v = tuwf->compile({ vndbid => 'g' })->validate($_[0][0]);
@@ -492,16 +497,20 @@ sub _validate_tag {
     $_[0][0] = $v->data;
     if($_[0]->@* == 2) { # compact form
         return 0 if !defined $_[0][1] || ref $_[0][1] || $_[0][1] !~ /^[0-9]+$/;
-        ($_[0][1],$_[0][2]) = ($_[0][1]%3, int($_[0][1]/3)/5);
+        ($_[0][1],$_[0][2],$_[0][3]) = ($_[0][1]%3, int($_[0][1]%(3*16)/3)/5, int($_[0][1]/3/16) == 1 ? 1 : 0);
     }
     # normalized form
-    return 0 if $_[0]->@* != 3;
+    return 0 if $_[0]->@* < 3 || $_[0]->@* > 4;
     return 0 if !defined $_[0][1] || ref $_[0][1] || $_[0][1] !~ /^[0-2]$/;
     return 0 if !defined $_[0][2] || ref $_[0][2] || $_[0][2] !~ /^(?:[0-2](?:\.[0-9]+)?|3(?:\.0+)?)$/;
+    if ($_[0]->@* == 4) {
+        return 0 if !defined $_[0][3] || ref $_[0][3] || $_[0][3] !~ /^[0-1]$/;
+        pop $_[0]->@* if !$_[0][3];
+    }
     1
 }
 
-sub _compact_tag { my $id = ($_->[0] =~ s/^g//r)*1; $_->[1] == 0 && $_->[2] == 0 ? $id : [ $id, int($_->[2]*5)*3 + $_->[1] ] }
+sub _compact_tag { my $id = ($_->[0] =~ s/^g//r)*1; $_->[1] == 0 && $_->[2] == 0 && !$_->[3] ? $id : [ $id, ($_->[3]?16*3:0) + int($_->[2]*5)*3 + $_->[1] ] }
 sub _compact_trait { my $id = ($_->[0] =~ s/^i//r)*1; $_->[1] == 0 ? $id : [ $id, int $_->[1] ] }
 
 # Accepts either $trait or [$trait, $maxspoil]. Normalizes to the latter.
@@ -620,15 +629,18 @@ sub _sql_where_tag {
     my($table) = @_;
     sub {
         my($neg, $all, $val) = @_;
-        my %f; # spoiler -> rating -> list
+        my %f; # spoiler -> rating -> lie -> list
         my @l;
-        push $f{$_->[1]}{$_->[2]}->@*, $_->[0] for @$val;
+        push $f{$_->[1]}{$_->[2]}{$_->[3]||''}->@*, $_->[0] for @$val;
         for my $s (keys %f) {
             for my $r (keys $f{$s}->%*) {
-                push @l, sql_and
-                    $s < 2 ? sql('spoiler <=', \$s) : (),
-                    $r > 0 ? sql('rating >=', \$r) : (),
-                    sql('tag IN', $f{$s}{$r});
+                for my $l (keys $f{$s}{$r}->%*) {
+                    push @l, sql_and
+                        $s < 2 ? sql('spoiler <=', \$s) : (),
+                        $r > 0 ? sql('rating >=', \$r) : (),
+                        $l ? ('NOT lie') : (),
+                        sql('tag IN', $f{$s}{$r}{$l});
+                }
             }
         }
         sql 'v.id', $neg ? 'NOT' : (), 'IN(SELECT vid FROM', $table, 'WHERE', sql_or(@l), $all && @$val > 1 ? ('GROUP BY vid HAVING COUNT(tag) =', \scalar @$val) : (), ')'

sql/func.sql
@@ -296,20 +296,20 @@ BEGIN
     DELETE FROM tags_vn_inherit WHERE vid = uvid;
   END IF;
 
-  INSERT INTO tags_vn_inherit (tag, vid, rating, spoiler)
+  INSERT INTO tags_vn_inherit (tag, vid, rating, spoiler, lie)
     -- Add parent tags to each row in tags_vn_direct
-    WITH RECURSIVE t_all(lvl, tag, vid, vote, spoiler) AS (
-        SELECT 15, tag, vid, rating, spoiler
+    WITH RECURSIVE t_all(lvl, tag, vid, vote, spoiler, lie) AS (
+        SELECT 15, tag, vid, rating, spoiler, lie
           FROM tags_vn_direct
          WHERE (uvid IS NULL OR vid = uvid)
         UNION ALL
-        SELECT ta.lvl-1, tp.parent, ta.vid, ta.vote, ta.spoiler
+        SELECT ta.lvl-1, tp.parent, ta.vid, ta.vote, ta.spoiler, ta.lie
           FROM t_all ta
           JOIN tags_parents tp ON tp.id = ta.tag
          WHERE ta.lvl > 0
     )
     -- Merge duplicates
-    SELECT tag, vid, AVG(vote), MIN(spoiler)
+    SELECT tag, vid, AVG(vote), MIN(spoiler), bool_and(lie)
       FROM t_all
      WHERE tag IN(SELECT id FROM tags WHERE searchable)
      GROUP BY tag, vid;

sql/schema.sql
@@ -882,7 +882,8 @@ CREATE TABLE tags_vn_inherit (
   tag     vndbid NOT NULL,
   vid     vndbid NOT NULL,
   rating  real NOT NULL,
-  spoiler smallint NOT NULL
+  spoiler smallint NOT NULL,
+  lie     boolean NOT NULL
 );
 
 -- threads

util/updates/2022-10-22-tags_vn_inherit-lie.sql
@@ -1,0 +1,4 @@
+ALTER TABLE tags_vn_inherit ADD COLUMN lie boolean;
+\i sql/func.sql
+SELECT tag_vn_calc(null);
+ALTER TABLE tags_vn_inherit ALTER COLUMN lie DROP NOT NULL;

commit c42aaa4a91117329b178d8db4b9b7571ab44385a

VN::List: Fix handling of invalid filters
Broken in 138944ced5a32236afa479c418a33dc7ae8cf9b5 - I made that change
for debugging purposes, wasn't supposed to be committed. -.-

lib/VNWeb/VN/List.pm
@@ -234,7 +234,7 @@ TUWF::get qr{/v(?:/(?<char>all|[a-z0]))?}, sub {
         q => { onerror => undef },
         sq=> { onerror => undef },
         p => { upage => 1 },
-        f => { advsearch => 'v' },
+        f => { advsearch_err => 'v' },
         s => { tableopts => $TABLEOPTS },
         ch=> { onerror => [], type => 'array', scalar => 1, values => { onerror => undef, enum => ['0', 'a'..'z'] } },
         fil  => { required => 0 },