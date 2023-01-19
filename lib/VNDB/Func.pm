xso4/vndb-mirror

commit 31e320adb70a09f3222e8a0a667b64c0c201aee9

Use Perl built-in utf8 functions rather than "Encode" module
Same as TUWF commit 1a32e51a1156266ccb5257dc7f53dd3923cc7e67

Saves about 1MB on resident memory use, whooptiedoo. \o/
(Only fixed this for vndb.pl, Multi still has the Encode module loaded,
w/e)

lib/VNDB/Func.pm
@@ -7,7 +7,6 @@ use Exporter 'import';
 use POSIX 'strftime', 'floor';
 use Socket 'inet_pton', 'inet_ntop', 'AF_INET', 'AF_INET6';
 use Digest::SHA 'sha1';
-use Encode 'encode_utf8';
 use VNDB::Config;
 use VNDB::Types;
 use VNDB::BBCode;
@@ -297,7 +296,7 @@ sub md2html {
 
 
 sub is_insecurepass {
-    my $hash = sha1 encode_utf8 shift;
+    my $hash = sha1 utf8::encode(local $_=shift);
     my $dir = config->{root}.'/data/hibp';
     return 0 if !-d $dir;
 

lib/VNWeb/User/Edit.pm
@@ -5,7 +5,6 @@ use VNDB::Skins;
 use VNWeb::LangPref;
 
 use Digest::SHA 'sha1';
-use Encode 'encode_utf8';
 
 
 my $FORM = {
@@ -185,7 +184,7 @@ elm_api UserEdit => $FORM_OUT, $FORM_IN, sub {
             vnrel_langs vnrel_olang vnrel_mtl staffed_langs staffed_olang staffed_unoff
             spoilers skin customcss title_langs alttitle_langs
         /;
-        $setp{customcss_csum} = length $p->{customcss} ? unpack 'q', sha1 encode_utf8 $p->{customcss} : 0;
+        $setp{customcss_csum} = length $p->{customcss} ? unpack 'q', sha1 utf8::encode(local $_=$p->{customcss}) : 0;
         tuwf->dbExeci('DELETE FROM users_traits WHERE id =', \$data->{id});
         tuwf->dbExeci('INSERT INTO users_traits', { id => $data->{id}, tid => $_->{tid} }) for $p->{traits}->@*;
 

lib/VNWeb/Auth.pm
@@ -27,7 +27,6 @@ use Carp 'croak';
 use Digest::SHA qw|sha1 sha1_hex|;
 use Crypt::URandom 'urandom';
 use Crypt::ScryptKDF 'scrypt_raw';
-use Encode 'encode_utf8';
 use MIME::Base64 'encode_base64url';
 
 use VNDB::Func 'norm_ip';
@@ -114,7 +113,8 @@ sub _preparepass {
     my($self, $pass, $salt, $N, $r, $p) = @_;
     ($N, $r, $p) = @{$self->{scrypt_args}} if !$N;
     $salt ||= urandom(8);
-    unpack 'H*', pack 'NCCa8a*', $N, $r, $p, $salt, scrypt_raw(encode_utf8($pass), $self->{scrypt_salt} . $salt, $N, $r, $p, 32);
+    utf8::encode(my $utf8pass = $pass);
+    unpack 'H*', pack 'NCCa8a*', $N, $r, $p, $salt, scrypt_raw($utf8pass, $self->{scrypt_salt} . $salt, $N, $r, $p, 32);
 }
 
 

lib/VNWeb/Graph.pm
@@ -5,7 +5,6 @@ package VNWeb::Graph;
 use v5.26;
 use AnyEvent::Util;
 use TUWF::XML 'xml_escape';
-use Encode 'encode_utf8', 'decode_utf8';
 use Exporter 'import';
 use List::Util 'max';
 use VNDB::Config;
@@ -44,7 +43,7 @@ sub gen_nodes {
 sub dot2svg {
     my($dot) = @_;
 
-    $dot = encode_utf8 $dot;
+    utf8::encode $dot;
     my $e = run_cmd([config->{graphviz_path},'-Tsvg'], '<', \$dot, '>', \my $out, '2>', \my $err)->recv;
     warn "graphviz STDERR: $err\n" if chomp $err;
     $e and die "Failed to run graphviz";
@@ -55,8 +54,8 @@ sub dot2svg {
     # - Remove first <polygon> element (emulates a background color)
     # - Replace stroke and fill attributes with classes (so that coloring is done in CSS)
     # (I used to have an implementation based on XML::Parser, but regexes are so much faster...)
-    decode_utf8($out)
-        =~ s/<\?xml.+?\?>//r
+    utf8::decode $out or die;
+    $out=~ s/<\?xml.+?\?>//r
         =~ s/<!DOCTYPE[^>]*>//r
         =~ s/<!--.*?-->//srg
         =~ s/<title>.+?<\/title>//gr

lib/VNWeb/HTML.pm
@@ -4,7 +4,6 @@ use v5.26;
 use warnings;
 use utf8;
 use Algorithm::Diff::XS 'sdiff', 'compact_diff';
-use Encode 'encode_utf8', 'decode_utf8';
 use JSON::XS;
 use TUWF ':html5_', 'uri_escape', 'html_escape', 'mkclass';
 use Exporter 'import';
@@ -602,8 +601,8 @@ sub _revision_fmtcol_ {
             if($diff) {
                 my $lastchunk = int (($#$diff-2)/2);
                 for my $n (0..$lastchunk) {
-                    my $a = decode_utf8 join '', @{$old}[ $diff->[$n*2]   .. $diff->[$n*2+2]-1 ];
-                    my $b = decode_utf8 join '', @{$new}[ $diff->[$n*2+1] .. $diff->[$n*2+3]-1 ];
+                    utf8::decode(my $a = join '', @{$old}[ $diff->[$n*2]   .. $diff->[$n*2+2]-1 ]);
+                    utf8::decode(my $b = join '', @{$new}[ $diff->[$n*2+1] .. $diff->[$n*2+3]-1 ]);
 
                     # Difference, highlight and display in full
                     if($n % 2) {
@@ -672,8 +671,8 @@ sub _revision_diff_ {
 
         # Do a word-based diff if this is a large chunk of text, otherwise character-based.
         my $split = length $item->[1] > 1024 ? qr/([ ,\n]+)/ : qr//;
-        $item->[1] = [map encode_utf8($_), split $split, $item->[1]];
-        $item->[2] = [map encode_utf8($_), split $split, $item->[2]];
+        $item->[1] = [map { utf8::encode($_); $_ } split $split, $item->[1]];
+        $item->[2] = [map { utf8::encode($_); $_ } split $split, $item->[2]];
         $item->[3] = compact_diff $item->[1], $item->[2];
     }
 

commit 94c5b27789677ab8bf1f04168f0380a698934d6c

Fix HIBP password check again
Broken in 31e320adb70a09f3222e8a0a667b64c0c201aee9
(Also CSS checksum generation, but that's not a big problem)

lib/VNDB/Func.pm
@@ -296,7 +296,8 @@ sub md2html {
 
 
 sub is_insecurepass {
-    my $hash = sha1 utf8::encode(local $_=shift);
+    utf8::encode(local $_ = shift);
+    my $hash = sha1 $_;
     my $dir = config->{root}.'/data/hibp';
     return 0 if !-d $dir;
 

lib/VNWeb/User/Edit.pm
@@ -191,7 +191,7 @@ elm_api UserEdit => $FORM_OUT, $FORM_IN, sub {
             vnrel_langs vnrel_olang vnrel_mtl staffed_langs staffed_olang staffed_unoff
             spoilers skin customcss timezone title_langs alttitle_langs
         /;
-        $setp{customcss_csum} = length $p->{customcss} ? unpack 'q', sha1 utf8::encode(local $_=$p->{customcss}) : 0;
+        $setp{customcss_csum} = length $p->{customcss} ? unpack 'q', sha1 do { utf8::encode(local $_=$p->{customcss}); $_ } : 0;
         tuwf->dbExeci('DELETE FROM users_traits WHERE id =', \$data->{id});
         tuwf->dbExeci('INSERT INTO users_traits', { id => $data->{id}, tid => $_->{tid} }) for $p->{traits}->@*;
 