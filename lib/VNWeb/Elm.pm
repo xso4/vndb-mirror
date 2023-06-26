xso4/vndb-mirror

commit 689cc204cfbb1bd63032be9c58a7e4f4e30125db

Types/JS: Move language properties into VNDB::Types + improve lang sorting in dropdown selector

lib/VNWeb/Elm.pm
@@ -1,4 +1,4 @@
-# This module is responsible for generating elm/Gen/*.
+# This module is responsible for generating elm/Gen/*;
 #
 # It exports an `elm_api` function to create an API endpoint, type definitions,
 # a JSON encoder and HTML5 validation attributes to simplify and synchronize
@@ -437,7 +437,7 @@ sub write_types {
 
     $data .= def adminEMail => String => string config->{admin_email};
     $data .= def skins      => 'List (String, String)' => list map tuple(string $_, string skins->{$_}{name}), sort { skins->{$a}{name} cmp skins->{$b}{name} } keys skins->%*;
-    $data .= def languages  => 'List (String, String)' => list map tuple(string $_, string $LANGUAGE{$_}), sort { $LANGUAGE{$a} cmp $LANGUAGE{$b} } keys %LANGUAGE;
+    $data .= def languages  => 'List (String, String)' => list map tuple(string $_, string $LANGUAGE{$_}{txt}), sort { $LANGUAGE{$a} cmp $LANGUAGE{$b} } keys %LANGUAGE;
     $data .= def platforms  => 'List (String, String)' => list map tuple(string $_, string $PLATFORM{$_}), keys %PLATFORM;
     $data .= def releaseTypes => 'List (String, String)' => list map tuple(string $_, string $RELEASE_TYPE{$_}), keys %RELEASE_TYPE;
     $data .= def media      => 'List (String, String, Bool)' => list map tuple(string $_, string $MEDIUM{$_}{txt}, $MEDIUM{$_}{qty}?'True':'False'), keys %MEDIUM;

js/basic/ds.js
@@ -309,12 +309,18 @@ DS.Engines = {
     view: obj => [ obj.id, m('small', ' ('+obj.count+')') ],
 };
 
-// TODO: Ranking? Popular langs, prefix match?
 DS.Lang = {
     opts: { width: 250 },
     list: (src, str, cb) => cb(vndbTypes.language
         .filter(([id,label]) => str === id.toLowerCase() || label.toLowerCase().includes(str))
-        .map(([id,label]) => ({id,label}))),
+        // Sorting considerations: id match > prefix match > rank > label
+        .sort(([aid,alabel,,arank],[bid,blabel,,brank]) =>
+            aid === bid ? 0 : aid.toLowerCase() === str ? -1 : bid.toLowerCase() === str ? 1
+            : alabel.toLowerCase().startsWith(str) && !blabel.toLowerCase().startsWith(str) ? -1
+            : !alabel.toLowerCase().startsWith(str) && blabel.toLowerCase().startsWith(str) ? 1
+            : brank - arank) // sort() is stable so no need to compare label
+        .map(([id,label]) => ({id,label}))
+    ),
     view: obj => [ LangIcon(obj.id), obj.label ]
 };
 

js/user/UserEdit.js
@@ -183,11 +183,10 @@ const Traits = initVnode => {
 };
 
 
-const romanized_langs = Object.fromEntries([ '', 'ar', 'fa', 'he', 'hi', 'ja', 'ko', 'ru', 'sk', 'th', 'uk', 'ur', 'zh', 'zh-Hans', 'zh-Hant' ].map(e => ([e,1])));
-
 const Titles = initVnode => {
     const lst = initVnode.attrs.lst;
     const langs = Object.fromEntries(vndbTypes.language);
+    const nonlatin = Object.fromEntries(vndbTypes.language.filter(l => !l[2]).map(l => [l[0],true]).concat([['',true]]));
     const ds = new DS(DS.Lang, { onselect: obj => {
         const o = lst.pop();
         lst.push({lang: obj.id, latin: false, official: true });
@@ -197,7 +196,7 @@ const Titles = initVnode => {
         m('tbody', lst.map((t,n) => m('tr',
             m('td', '#'+(n+1)),
             m('td', t.lang ? [LangIcon(t.lang), langs[t.lang]] : ['Original language']),
-            m('td', romanized_langs[t.lang || ''] ? m('label',
+            m('td', nonlatin[t.lang || ''] ? m('label',
                 m('input[type=checkbox]', { checked: t.latin, oninput: ev => t.latin = ev.target.checked }),
                 ' romanized'
             ) : null),

lib/VNDB/Types.pm
@@ -15,58 +15,60 @@ sub hash {
 
 
 # SQL: ENUM language
+# 'latin' indicates whether the language is primarily written in a latin-ish script.
+# 'rank' is for quick selection of commonly used languages.
 hash LANGUAGE =>
-    ar => 'Arabic',
-    bg => 'Bulgarian',
-    ca => 'Catalan',
-    cs => 'Czech',
-    ck => 'Cherokee', # 'chr' in ISO 639-2 but not present in ISO 639-1, let's just use an unassigned code
-    da => 'Danish',
-    de => 'German',
-    el => 'Greek',
-    en => 'English',
-    eo => 'Esperanto',
-    es => 'Spanish',
-    eu => 'Basque',
-    fa => 'Persian',
-    fi => 'Finnish',
-    fr => 'French',
-    ga => 'Irish',
-    gd => 'Scottish Gaelic',
-    he => 'Hebrew',
-    hi => 'Hindi',
-    hr => 'Croatian',
-    hu => 'Hungarian',
-    id => 'Indonesian',
-    it => 'Italian',
-    iu => 'Inuktitut',
-    ja => 'Japanese',
-    ko => 'Korean',
-    mk => 'Macedonian',
-    ms => 'Malay',
-    la => 'Latin',
-    lt => 'Lithuanian',
-    lv => 'Latvian',
-    nl => 'Dutch',
-    no => 'Norwegian',
-    pl => 'Polish',
-    'pt-br' => 'Portuguese (Brazil)',
-    'pt-pt' => 'Portuguese (Portugal)',
-    ro => 'Romanian',
-    ru => 'Russian',
-    sk => 'Slovak',
-    sl => 'Slovene',
-    sr => 'Serbian',
-    sv => 'Swedish',
-    ta => 'Tagalog',
-    th => 'Thai',
-    tr => 'Turkish',
-    uk => 'Ukrainian',
-    ur => 'Urdu',
-    vi => 'Vietnamese',
-    zh => 'Chinese',
-    'zh-Hans' => 'Chinese (simplified)',
-    'zh-Hant' => 'Chinese (traditional)';
+    ar       => { latin => 0, rank => 0, txt => 'Arabic' },
+    eu       => { latin => 1, rank => 0, txt => 'Basque' },
+    bg       => { latin => 1, rank => 0, txt => 'Bulgarian' },
+    ca       => { latin => 1, rank => 0, txt => 'Catalan' },
+    ck       => { latin => 0, rank => 0, txt => 'Cherokee' }, # 'chr' in ISO 639-2 but not present in ISO 639-1, let's just use an unassigned code
+    zh       => { latin => 0, rank => 2, txt => 'Chinese' },
+    'zh-Hans'=> { latin => 0, rank => 2, txt => 'Chinese (simplified)' },
+    'zh-Hant'=> { latin => 0, rank => 2, txt => 'Chinese (traditional)' },
+    hr       => { latin => 1, rank => 0, txt => 'Croatian' },
+    cs       => { latin => 1, rank => 0, txt => 'Czech' },
+    da       => { latin => 1, rank => 0, txt => 'Danish' },
+    nl       => { latin => 1, rank => 0, txt => 'Dutch' },
+    en       => { latin => 1, rank => 3, txt => 'English' },
+    eo       => { latin => 1, rank => 0, txt => 'Esperanto' },
+    fi       => { latin => 1, rank => 0, txt => 'Finnish' },
+    fr       => { latin => 1, rank => 1, txt => 'French' },
+    de       => { latin => 1, rank => 1, txt => 'German' },
+    el       => { latin => 0, rank => 0, txt => 'Greek' },
+    he       => { latin => 0, rank => 0, txt => 'Hebrew' },
+    hi       => { latin => 0, rank => 0, txt => 'Hindi' },
+    hu       => { latin => 1, rank => 0, txt => 'Hungarian' },
+    ga       => { latin => 1, rank => 0, txt => 'Irish' },
+    id       => { latin => 1, rank => 0, txt => 'Indonesian' },
+    it       => { latin => 1, rank => 0, txt => 'Italian' },
+    iu       => { latin => 1, rank => 0, txt => 'Inuktitut' },
+    ja       => { latin => 0, rank => 4, txt => 'Japanese' },
+    ko       => { latin => 0, rank => 1, txt => 'Korean' },
+    la       => { latin => 1, rank => 0, txt => 'Latin' },
+    lv       => { latin => 1, rank => 0, txt => 'Latvian' },
+    lt       => { latin => 1, rank => 0, txt => 'Lithuanian' },
+    mk       => { latin => 1, rank => 0, txt => 'Macedonian' },
+    ms       => { latin => 1, rank => 0, txt => 'Malay' },
+    no       => { latin => 1, rank => 0, txt => 'Norwegian' },
+    fa       => { latin => 0, rank => 0, txt => 'Persian' },
+    pl       => { latin => 1, rank => 0, txt => 'Polish' },
+    'pt-br'  => { latin => 1, rank => 1, txt => 'Portuguese (Brazil)' },
+    'pt-pt'  => { latin => 1, rank => 1, txt => 'Portuguese (Portugal)' },
+    ro       => { latin => 1, rank => 0, txt => 'Romanian' },
+    ru       => { latin => 0, rank => 2, txt => 'Russian' },
+    gd       => { latin => 1, rank => 0, txt => 'Scottish Gaelic' },
+    sr       => { latin => 1, rank => 0, txt => 'Serbian' },
+    sk       => { latin => 0, rank => 0, txt => 'Slovak' },
+    sl       => { latin => 1, rank => 0, txt => 'Slovene' },
+    es       => { latin => 1, rank => 1, txt => 'Spanish' },
+    sv       => { latin => 1, rank => 0, txt => 'Swedish' },
+    ta       => { latin => 1, rank => 0, txt => 'Tagalog' },
+    th       => { latin => 0, rank => 0, txt => 'Thai' },
+    tr       => { latin => 1, rank => 0, txt => 'Turkish' },
+    uk       => { latin => 0, rank => 1, txt => 'Ukrainian' },
+    ur       => { latin => 0, rank => 0, txt => 'Urdu' },
+    vi       => { latin => 1, rank => 1, txt => 'Vietnamese' };
 
 
 

lib/VNWeb/API.pm
@@ -449,7 +449,7 @@ sub proc_results {
 api_get '/schema', {}, sub {
     state $s = {
         enums => {
-            language => [ map +{ id => $_, label => $LANGUAGE{$_} }, keys %LANGUAGE ],
+            language => [ map +{ id => $_, label => $LANGUAGE{$_}{txt} }, keys %LANGUAGE ],
             platform => [ map +{ id => $_, label => $PLATFORM{$_} }, keys %PLATFORM ],
             medium   => [ map +{ id => $_, label => $MEDIUM{$_}{txt}, plural => $MEDIUM{$_}{plural}||undef }, keys %MEDIUM ],
         },

lib/VNWeb/Misc/HomePage.pm
@@ -193,7 +193,7 @@ sub releases_ {
                 rdate_ $_->{released};
                 txt_ ' ';
                 platform_ $_ for $_->{plat}->@*;
-                abbr_ class => "icon-lang-$_", title => $LANGUAGE{$_}, '' for $_->{lang}->@*;
+                abbr_ class => "icon-lang-$_", title => $LANGUAGE{$_}{txt}, '' for $_->{lang}->@*;
                 txt_ ' ';
                 a_ href => "/$_->{id}", tattr $_;
             }

lib/VNWeb/Producers/Graph.pm
@@ -36,7 +36,7 @@ TUWF::get qr{/$RE{pid}/rg}, sub {
             qq|n$n->{id} [ $nodeid URL = "/$n->{id}", tooltip = "$tooltip", label=<|.
             qq|<TABLE CELLSPACING="0" CELLPADDING="2" BORDER="0" CELLBORDER="1" BGCOLOR="#222222">|.
             qq|<TR><TD COLSPAN="2" ALIGN="CENTER" CELLPADDING="3"><FONT POINT-SIZE="9">  $name  </FONT></TD></TR>|.
-            qq|<TR><TD ALIGN="CENTER"> $LANGUAGE{$n->{lang}} </TD><TD ALIGN="CENTER"> $PRODUCER_TYPE{$n->{type}} </TD></TR>|.
+            qq|<TR><TD ALIGN="CENTER"> $LANGUAGE{$n->{lang}}{txt} </TD><TD ALIGN="CENTER"> $PRODUCER_TYPE{$n->{type}} </TD></TR>|.
             qq|</TABLE>> ]|;
 
         push @lines, node_more $n->{id}, "/$n->{id}/rg$params", scalar grep !$nodes->{$_}, $n->{rels}->@*;

lib/VNWeb/Producers/List.pm
@@ -14,7 +14,7 @@ sub listing_ {
         h1_ $opt->{q} ? 'Search results' : 'Browse producers';
         ul_ sub {
             li_ sub {
-                abbr_ class => "icon-lang-$_->{lang}", title => $LANGUAGE{$_->{lang}}, '';
+                abbr_ class => "icon-lang-$_->{lang}", title => $LANGUAGE{$_->{lang}}{txt}, '';
                 a_ href => "/$_->{id}", tattr $_;
             } for @$list;
         }

commit cac41c8361194bd1d010357a946f056454231509

Elm: Fix sorting of language list
https://vndb.org/t950.1541 pointed out I had broken that in
689cc204cfbb1bd63032be9c58a7e4f4e30125db

lib/VNWeb/Elm.pm
@@ -427,7 +427,7 @@ sub write_types {
     my $data = '';
 
     $data .= def adminEMail => String => string config->{admin_email};
-    $data .= def languages  => 'List (String, String)' => list map tuple(string $_, string $LANGUAGE{$_}{txt}), sort { $LANGUAGE{$a} cmp $LANGUAGE{$b} } keys %LANGUAGE;
+    $data .= def languages  => 'List (String, String)' => list map tuple(string $_, string $LANGUAGE{$_}{txt}), sort { $LANGUAGE{$a}{txt} cmp $LANGUAGE{$b}{txt} } keys %LANGUAGE;
     $data .= def platforms  => 'List (String, String)' => list map tuple(string $_, string $PLATFORM{$_}), keys %PLATFORM;
     $data .= def releaseTypes => 'List (String, String)' => list map tuple(string $_, string $RELEASE_TYPE{$_}), keys %RELEASE_TYPE;
     $data .= def media      => 'List (String, String, Bool)' => list map tuple(string $_, string $MEDIUM{$_}{txt}, $MEDIUM{$_}{qty}?'True':'False'), keys %MEDIUM;