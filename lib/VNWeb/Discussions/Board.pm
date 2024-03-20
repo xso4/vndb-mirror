xso4/vndb-mirror

commit 8a8593e91e5252a24fdfa7d46c28134668ab7c9c

Add support for account soft-deletion and email opt-out list
This work is to support a safe self-service account deletion feature,
but I haven't implemented that part yet.

lib/VNWeb/Discussions/UPosts.pm
@@ -36,7 +36,7 @@ sub listing_ {
 
 TUWF::get qr{/$RE{uid}/posts}, sub {
     my $u = tuwf->dbRowi('SELECT id, ', sql_user(), 'FROM users u WHERE id =', \tuwf->capture('id'));
-    return tuwf->resNotFound if !$u->{id};
+    return tuwf->resNotFound if !$u->{id} || (!$u->{username} && !auth->isMod);
 
     my $page = tuwf->validate(get => p => { upage => 1 })->data;
 

lib/VNWeb/Reviews/List.pm
@@ -51,7 +51,7 @@ TUWF::get qr{/w}, sub {
     $opt->{s} = 'id' if $opt->{s} eq 'rating' && !auth->isMod;
 
     my $u = $opt->{u} && tuwf->dbRowi('SELECT id, ', sql_user(), 'FROM users u WHERE id =', \$opt->{u});
-    return tuwf->resNotFound if $u && !$u->{id};
+    return tuwf->resNotFound if $u && (!$u->{id} || (!$u->{username} && !auth->isMod));
 
     my $where = sql_and
         $u ? sql 'w.uid =', \$u->{id} : (),

lib/VNWeb/User/Page.pm
@@ -23,7 +23,8 @@ sub _info_table_ {
             auth->permUsermod ? () : 'AND date > NOW()-\'1 month\'::interval', 'ORDER BY date DESC');
         td_ class => 'key', 'Username';
         td_ sub {
-            txt_ $u->{user_name};
+            txt_ $u->{user_name} if defined $u->{user_name};
+            b_ 'Account deleted' if !defined $u->{user_name};
             user_maybebanned_ $u;
             txt_ ' ('; a_ href => "/$u->{id}", $u->{id};
             txt_ ')';
@@ -197,7 +198,7 @@ TUWF::get qr{/$RE{uid}}, sub {
           FROM users u
          WHERE id =}, \tuwf->capture('id')
     );
-    return tuwf->resNotFound if !$u->{id};
+    return tuwf->resNotFound if !$u->{id} || (!$u->{username} && !auth->isMod);
 
     my $own = (auth && auth->uid eq $u->{id}) || auth->permUsermod;
 

lib/VNWeb/Images/List.pm
@@ -154,7 +154,7 @@ TUWF::get qr{/img/list}, sub {
     $opt->{d} = 0 if !$opt->{u};
 
     my $u = $opt->{u} && tuwf->dbRowi('SELECT id, ', sql_user(), 'FROM users u WHERE id =', \$opt->{u});
-    return tuwf->resNotFound if $opt->{u} && !$u->{user_id};
+    return tuwf->resNotFound if $opt->{u} && (!$u->{id} || (!defined $u->{user_name} && !auth->isMod));
 
     my $where = sql_and
         $opt->{t}->@* ? sql_or(map sql('i.id BETWEEN vndbid(',\"$_",',1) AND vndbid_max(',\"$_",')'), $opt->{t}->@*) : (),

lib/VNWeb/User/List.pm
@@ -67,6 +67,8 @@ TUWF::get qr{/u/(?<char>[0a-z]|all)}, sub {
     )->data;
 
     my @where = (
+        'username IS NOT NULL',
+        auth->permUsermod ? () : 'email_confirmed',
         $char eq 'all' ? () : sql('match_firstchar(username, ', \$char, ')'),
         $opt->{q} ? sql_or(
             auth->permUsermod && $opt->{q} =~ /@/ ? sql('id IN(SELECT uid FROM user_emailtoid(', \$opt->{q}, '))') : (),
@@ -106,6 +108,8 @@ TUWF::get qr{/u/(?<char>[0a-z]|all)}, sub {
                 a_ href => "/u/$_", $_ eq $char ? (class => 'optselected') : (), $_ eq 'all' ? 'ALL' : $_ ? uc $_ : '#'
                     for ('all', 'a'..'z', 0);
             };
+            b_ 'The given email address is on the opt-out list.'
+              if auth->permUsermod && $opt->{q} && $opt->{q} =~ /@/ && tuwf->dbVali('SELECT email_optout_check(', \$opt->{q}, ')');
         };
         listing_ $opt, $list, $count if $count;
     };

js/user/UserAdmin.js
@@ -15,7 +15,7 @@ widget('UserAdmin', initVnode => {
     };
     const view = () => m(Form, {api, onsubmit: () => api.call(data)},
         m('article',
-            m('h1', 'Admin settings for '+data.username),
+            m('h1', 'Admin settings for '+(data.username||data.id)),
             m('fieldset.form',
                 m('fieldset',
                     m('label', 'Preset'),

js/user/UserRegister.js
@@ -12,7 +12,9 @@ widget('UserRegister', vnode => {
         m('div.notice', m('p',
             'Your account has been created!', m('br'),
             'Check your inbox for an email with instructions to activate your account.', m('br'),
-            "(also make sure to check your spam box if it doesn't seem to be arriving)"
+            "(also make sure to check your spam box if it doesn't seem to be arriving)", m('br'),
+            m('br'),
+            "If the email does not arrive within a few hours, please send a mail to contact@vndb.org so we can investigate.",
         ))
     );
     const view = () => success ? donemsg : m(Form, {onsubmit, api}, m('article',

lib/VNWeb/HTML.pm
@@ -91,12 +91,14 @@ sub user_ {
     my $capital = shift;
     my sub f($) { $obj->{"${prefix}$_[0]"} }
 
-    return small_ 'anonymous' if !f 'id';
+    my $softdel = !defined f 'name';
+    return small_ 'anonymous' if ($softdel && !auth->isMod) || !f 'id';
     my $fancy = !(auth->pref('nodistract_can') && auth->pref('nodistract_nofancy'));
     my $uniname = f 'uniname_can' && f 'uniname';
     a_ href => '/'.f('id'),
+        $softdel ? (class => 'grayedout') : (),
         $fancy && $uniname ? (title => f('name'), $uniname) :
-        (!$fancy && $uniname ? (title => $uniname) : (), $capital ? f 'name' : f 'name');
+        (!$fancy && $uniname ? (title => $uniname) : (), ($capital ? f 'name' : f 'name') // f 'id');
     txt_ '⭐' if $fancy && f 'support_can' && f 'support_enabled';
     user_maybebanned_ $obj, $prefix;
 }
@@ -110,7 +112,7 @@ sub user_displayname {
 
     return 'anonymous' if !f 'id';
     my $fancy = !(auth->pref('nodistract_can') && auth->pref('nodistract_nofancy'));
-    $fancy && f 'uniname_can' && f 'uniname' ? f 'uniname' : f 'name'
+    $fancy && f 'uniname_can' && f 'uniname' ? f 'uniname' : f('name') // f 'id'
 }
 
 # Display a release date.

commit 6803a5b5f3dfbc42d4f831158b4a9ac1e56adfe9

Fix a few user pages not being accessible by non-mods
Broken in 8a8593e91e5252a24fdfa7d46c28134668ab7c9c
Oops.

lib/VNWeb/Discussions/Board.pm
@@ -13,6 +13,7 @@ TUWF::get qr{/t/(all|$BOARD_RE)}, sub {
 
     my $obj = $id ? dbobj $id : undef;
     return tuwf->resNotFound if $id && !$obj->{id};
+    return tuwf->resNotFound if $id && $id =~ /^u/ && $obj->{entry_hidden} && !auth->isMod;
 
     my $title = $obj ? "Related discussions for $obj->{title}[1]" : $type eq 'all' ? 'All boards' : $BOARD_TYPE{$type}{txt};
     my $createurl = '/t/'.($id || ($type eq 'db' ? 'db' : 'ge')).'/new';

lib/VNWeb/Discussions/UPosts.pm
@@ -36,7 +36,7 @@ sub listing_ {
 
 TUWF::get qr{/$RE{uid}/posts}, sub {
     my $u = tuwf->dbRowi('SELECT id, ', sql_user(), 'FROM users u WHERE id =', \tuwf->capture('id'));
-    return tuwf->resNotFound if !$u->{id} || (!$u->{username} && !auth->isMod);
+    return tuwf->resNotFound if !$u->{id} || (!$u->{user_name} && !auth->isMod);
 
     my $page = tuwf->validate(get => p => { upage => 1 })->data;
 

lib/VNWeb/Reviews/List.pm
@@ -51,7 +51,7 @@ TUWF::get qr{/w}, sub {
     $opt->{s} = 'id' if $opt->{s} eq 'rating' && !auth->isMod;
 
     my $u = $opt->{u} && tuwf->dbRowi('SELECT id, ', sql_user(), 'FROM users u WHERE id =', \$opt->{u});
-    return tuwf->resNotFound if $u && (!$u->{id} || (!$u->{username} && !auth->isMod));
+    return tuwf->resNotFound if $u && (!$u->{id} || (!$u->{user_name} && !auth->isMod));
 
     my $where = sql_and
         $u ? sql 'w.uid =', \$u->{id} : (),

lib/VNWeb/User/Page.pm
@@ -198,7 +198,7 @@ TUWF::get qr{/$RE{uid}}, sub {
           FROM users u
          WHERE id =}, \tuwf->capture('id')
     );
-    return tuwf->resNotFound if !$u->{id} || (!$u->{username} && !auth->isMod);
+    return tuwf->resNotFound if !$u->{id} || (!$u->{user_name} && !auth->isMod);
 
     my $own = (auth && auth->uid eq $u->{id}) || auth->permUsermod;
 