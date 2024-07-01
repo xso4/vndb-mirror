78eef304b19fadd0308729e411a0cbacaf01fe88
Yorhel <git@yorhel.nl>
2024-07-01 13:54:51 +0200

    SQL/search_norm_term(): Ignore ™, © and ®
    These symbols do not appear meaningfully in any title in the DB, so
    normalizing them to 'tm', 'c', and 'r' isn't particularly useful.

--- sql/util.sql
+++ sql/util.sql
@@ -83,7 +83,7 @@ $$ LANGUAGE SQL IMMUTABLE;
 -- Helper function for search normalization
 CREATE OR REPLACE FUNCTION search_norm_term(str text) RETURNS text AS $$
   SELECT regexp_replace(regexp_replace(regexp_replace(regexp_replace(regexp_replace(regexp_replace(
-            translate(lower(public.unaccent(normalize(str, NFKC))), $s$@,_-‐.~～〜∼ー῀:[]()%+!?#$`♥★☆♪†「」『』【】・<>'$s$, 'a'), -- '
+            translate(lower(public.unaccent(normalize(translate(str, '™©®', ''), NFKC))), $s$@,_-‐.~～〜∼ー῀:[]()%+!?#$`♥★☆♪†「」『』【】・<>'$s$, 'a'), -- '
             '\s+', '', 'g'),
             '&', 'and', 'g'),
             'disc', 'disk', 'g'),
