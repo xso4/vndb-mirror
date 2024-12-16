433d1cd967f1e199203f44607c15135563a058c4
Yorhel <git@yorhel.nl>
2024-12-16 11:35:20 +0100

    Fixups: Wikidata fetcher & rename update script

--- lib/Multi/Wikidata.pm
+++ lib/Multi/Wikidata.pm
@@ -37,7 +37,7 @@ sub fetch {
      WHERE id IN(
               SELECT l_wikidata FROM producers WHERE l_wikidata IS NOT NULL AND NOT hidden
         UNION SELECT l_wikidata FROM vn        WHERE l_wikidata IS NOT NULL AND NOT hidden
-        UNION SELECT value FROM extlinks WHERE site = 'wikidata' AND c_ref)
+        UNION SELECT value::int FROM extlinks WHERE site = 'wikidata' AND c_ref)
        AND (lastfetch IS NULL OR lastfetch < date_trunc('hour', now()-($1 * '1 second'::interval)))
      ORDER BY lastfetch NULLS FIRST
      LIMIT $2
