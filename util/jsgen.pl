xso4/vndb-mirror

commit 62ae2b4e320966dec40c1b1dfefba80584c1210e

JS/AdvSearch: Add extlinks filters (+ split shared extlinks list for edit forms)

util/jsgen.pl
@@ -7,10 +7,12 @@ use TUWF::Validate::Interop;
 use JSON::XS;
 use VNWeb::Validation ();
 use VNWeb::TimeZone;
-use VNDB::ExtLinks ();
+use VNDB::ExtLinks '%LINKS';
 use VNDB::Skins;
 use VNDB::Types;
 use VNDB::Func 'fmtrating';
+
+my @LINKS = grep $LINKS{$_}{regex}, sort keys %LINKS;
 
 my $js = JSON::XS->new->pretty->canonical;
 
@@ -47,6 +49,7 @@ sub types {
         charGender=> [ map [$_, $CHAR_GENDER{$_}], keys %CHAR_GENDER ],
         charRole  => [ map [$_, $CHAR_ROLE{$_}{txt}], keys %CHAR_ROLE ],
         cupSize   => [ map [$_, $CUP_SIZE{$_}], keys %CUP_SIZE ],
+        extLinks  => [ map [$_, $LINKS{$_}{ent}, $LINKS{$_}{label}], @LINKS ],
     }).";\n";
 }
 
@@ -59,22 +62,11 @@ sub vskins {
 }
 
 sub extlinks {
-    sub t($t) {
-        my $L = \%VNDB::ExtLinks::LINKS;
-        [ map +{
-            site    => $_,
-            label   => $L->{$_}{label},
-            fmt     => $L->{$_}{fmt},
-            patt    => [ split /(<[^>]+>)/, $L->{$_}{patt} || ($L->{$_}{fmt} =~ s/%s/<code>/rg =~ s/%[0-9]*d/<number>/rg) ],
-            $L->{$_}{regex} ? (regex => TUWF::Validate::Interop::_re_compat($L->{$_}{full_regex})) : (),
-            $L->{$_}{ent} =~ /\U$t/ ? (multi => 1) : (),
-        }, grep $L->{$_}{ent} =~ /$t/i, sort keys %$L ]
-    }
-    print 'window.extLinks = '.$js->encode({
-        release  => t('r'),
-        producer => t('p'),
-        staff    => t('s'),
-    }).";\n";
+    print 'window.extLinksExt = '.$js->encode([ map [
+        $_->{fmt},
+        [ split /(<[^>]+>)/, $_->{patt} || ($_->{fmt} =~ s/%s/<code>/rg =~ s/%[0-9]*d/<number>/rg) ],
+        TUWF::Validate::Interop::_re_compat($_->{full_regex}),
+    ], map $LINKS{$_}, @LINKS ])."\n";
 }
 
 if ($ARGV[0] eq 'types') { validations; types; }

Makefile
@@ -159,7 +159,7 @@ ${GEN}/d3.js:
 	$T FETCH
 	$Q curl -s 'https://d3js.org/d3.v7.min.js' -o $@
 
-${GEN}/types.js: util/jsgen.pl lib/VNDB/Types.pm lib/VNWeb/Validation.pm
+${GEN}/types.js: util/jsgen.pl lib/VNDB/Types.pm lib/VNDB/ExtLinks.pm lib/VNWeb/Validation.pm
 	util/jsgen.pl types >$@
 
 ${GEN}/user.js: util/jsgen.pl lib/VNWeb/TimeZone.pm

js/basic/AdvSearch.js
@@ -85,6 +85,22 @@ const langField = (id, opstyle, source, label, title, prefix='') => ({
     ],
 });
 
+const extlinksField = (id, type) => ({
+    label: 'External links',
+    ...setField(id, 'set', {
+        list: (src, str, cb) => cb(vndbTypes
+            .extLinks.map(([id,ent,lbl]) => ({id,ent,lbl}))
+            .filter(o => o.ent.toLowerCase().includes(type))),
+        view: o => o.lbl,
+        opts: { width: 210, nosearch: true, maxCols: 2 }
+    }),
+    button: inst => inst.values.size === 0 ? m('small', 'External links') : [
+        opFmt(inst.op, true), ' ',
+        inst.values.size === 1 ? vndbTypes.extLinks.find(([id]) => id === inst.values.keys().next().value)[2]
+                               : 'Links ('+inst.values.size+')'
+    ],
+});
+
 const platformField = { // Works for both VNs and releases
     label: 'Platform',
     title: 'Platform availability',
@@ -348,7 +364,9 @@ regType('r', 'Release', [
     simpleSetField(12, 'eq', vndbTypes.voiced.map((v,i) => [i,v]), 'Voiced'),
     simpleSetField(13, 'eq', vndbTypes.animated.map((v,i) => [i,v,'Ero: '+v]), 'Ero animation'),
     simpleSetField(14, 'eq', vndbTypes.animated.map((v,i) => [i,v,'Story: '+v]), 'Story animation'),
-    // TODO: engine, DRM, extlinks, my list
+    // TODO: engine, DRM
+    extlinksField(19, 'r'),
+    // my list
 ]),
 
 regType('c', 'Char', [
@@ -371,14 +389,14 @@ regType('s', 'Staff', [
     langField(2, 'eq', DS.LocLang, 'Language', 'Primary language of the staff'),
     simpleSetField(4, 'eq', [['','Unknown','Gender: unknown'],['m','Male'],['f','Female']], 'Gender'),
     simpleSetField(5, 'set', [['seiyuu', 'Voice actor']].concat(vndbTypes.creditType), 'Role'),
-    // TODO: extlinks
+    extlinksField(6, 's'),
 ]);
 
 regType('p', 'Producer', [
     // TODO: name
     langField(2, 'eq', DS.LocLang, 'Language', 'Primary language of the producer'),
     simpleSetField(4, 'eq', vndbTypes.producerType, 'Type'),
-    // TODO: extlinks
+    extlinksField(5, 'p'),
 ]);
 
 

js/contrib/ProducerEdit.js
@@ -30,7 +30,7 @@ widget('ProducerEdit', initVnode => {
                 attrs: { id: 'description', rows: 6, maxlength: 5000 },
             }),
         ),
-        m(ExtLinks, {type: 'producer', links: data.extlinks}),
+        m(ExtLinks, {type: 'p', links: data.extlinks}),
     ];
 
     const prod = new DS(DS.Producers, {

js/contrib/ReleaseEdit.js
@@ -659,7 +659,7 @@ widget('ReleaseEdit', initVnode => {
                 m('label[for=catalog]', 'Catalog number'),
                 m(Input, { id: 'catalog', class: 'mw', maxlength: 50, data, field: 'catalog' }),
             ),
-            m(ExtLinks, {type: 'release', links: data.extlinks}),
+            m(ExtLinks, {type: 'r', links: data.extlinks}),
         ),
         m('fieldset.form',
             m('legend', 'Database relations'),

js/contrib/StaffEdit.js
@@ -95,7 +95,7 @@ widget('StaffEdit', initVnode => {
             m('p', 'Producer entry for this person, if there is one.'),
             m('p', 'Only set this if the producer and this staff entry are one and the same, not just when this staff happens to work for the producer.'),
         ),
-        m(ExtLinks, {type: 'staff', links: data.extlinks}),
+        m(ExtLinks, {type: 's', links: data.extlinks}),
         m('fieldset',
             m('label[for=description]', 'Notes / Biography'),
             m(TextPreview, {

js/contrib/index.js
@@ -96,9 +96,15 @@ const EditSum = vnode => {
 
 const ExtLinks = initVnode => {
     const links = initVnode.attrs.links;
-    const extlinks = extLinks[initVnode.attrs.type];
+    const extlinks = vndbTypes.extLinks.flatMap(
+        ([site,ent,label],i) => ent.toLowerCase().includes(initVnode.attrs.type) ? [{
+            site, label,
+            multi: ent.includes(initVnode.attrs.type.toUpperCase()),
+            fmt: extLinksExt[i][0],
+            patt: extLinksExt[i][1],
+            regex: extLinksExt[i][2],
+        }] : []);
     const extlinksMap = Object.fromEntries(extlinks.map(x => [x.site,x]));
-    const hasWeb = extlinks.find(l => l.site === 'website');
     const split = (fmt,v) => fmt.split(/(%[0-9]*[sd])/)
         .map((p,i) => i !== 1 ? p : String(v).padStart(p.match(/%(?:0([0-9]+))?/)[1]||0, '0'));
 
@@ -114,7 +120,7 @@ const ExtLinks = initVnode => {
     };
     const set = (o,v) => {
         if (v !== null) o.str = v;
-        o.lnk = extlinks.find(l => l.regex && new RegExp(l.regex).test(o.str));
+        o.lnk = extlinks.find(l => new RegExp(l.regex).test(o.str));
         o.val = o.lnk && o.str.match(new RegExp(o.lnk.regex)).filter(x => x !== undefined)[1];
         o.dup = o.lnk && links.find(l => l.site === o.lnk.site && l.value === o.val);
         if (o.lnk && !o.dup && (o.lnk.multi || !links.find(l => l.site === o.lnk.site))) add(o);
@@ -126,7 +132,7 @@ const ExtLinks = initVnode => {
             m('p', m('input[type=button][value=Update]', { onclick: () => add(o) }), m('span.invalid', ' URL recognized as: ', o.lnk.label)),
             m('p.invalid', 'Did you mean to update the URL?'),
         ] : null;
-    const Website = () => hasWeb ? m('fieldset',
+    const Website = () => extlinksMap.website ? m('fieldset',
         m('label[for=website]', 'Website'),
         m(Input, { id: 'website', class: 'xw', type: 'weburl', data: web, field: 'str', oninput: v => {
             const l = links.find(l => l.site === 'website');
@@ -139,7 +145,7 @@ const ExtLinks = initVnode => {
 
     const view = () => [ Website(), m('fieldset',
         m('label[for=extlinks]', 'External links', HelpButton('extlinks')),
-        m('table', links.filter(l => extlinksMap[l.site] && extlinksMap[l.site].regex).map(l => m('tr', {key: l.site+'-'+l.value},
+        m('table', links.filter(l => extlinksMap[l.site]).map(l => m('tr', {key: l.site+'-'+l.value},
             m('td', m(Button.Del, {onclick: () => { links.splice(links.indexOf(l), 1); set(inp,null)}})),
             m('td', m('a[target=_blank]', { href: split(extlinksMap[l.site].fmt, l.value).join('') }, extlinksMap[l.site].label)),
             m('td', split(extlinksMap[l.site].fmt, l.value).map((p,i) => m(i === 1 ? 'span' : 'small', p))),
@@ -150,7 +156,7 @@ const ExtLinks = initVnode => {
         ),
         Help('extlinks',
             m('p', 'Links to external websites. The following sites and URL formats are supported:'),
-            m('dl', extlinks.filter(l => extlinksMap[l.site] && extlinksMap[l.site].regex).flatMap(e => [
+            m('dl', extlinks.filter(l => extlinksMap[l.site]).flatMap(e => [
                 m('dt', e.label),
                 m('dd', e.patt.map((p,i) => m(i % 2 ? 'strong' : 'span', p))),
             ])),

lib/VNDB/ExtLinks.pm
@@ -6,6 +6,7 @@ use VNDB::Schema;
 use Exporter 'import';
 
 our @EXPORT = ('enrich_vislinks');
+our @EXPORT_OK = ('%LINKS');
 
 
 # column name in wikidata table => \%info
@@ -483,7 +484,7 @@ our %LINKS = (
         , regex => qr{(?:www\.)?wikidata\.org/wiki/(?:Special:EntityPage/)?Q$int}
         },
     wp => # Deprecated, replaced with wikidata
-        { ent => 'rsp'
+        { ent => 'sp'
         , label => 'Wikipedia'
         , fmt => 'https://en.wikipedia.org/wiki/%s'
         },

commit 28073cfa1c8fa2d4c2764d80f122c8c805899e07

JS/ExtLinks: Bring back website field
Broken in 62ae2b4e320966dec40c1b1dfefba80584c1210e
Fixes https://vndb.org/t2520.964

util/jsgen.pl
@@ -12,7 +12,7 @@ use VNDB::Skins;
 use VNDB::Types;
 use VNDB::Func 'fmtrating';
 
-my @LINKS = grep $LINKS{$_}{regex}, sort keys %LINKS;
+my @LINKS = grep $_ eq 'website' || $LINKS{$_}{regex}, sort keys %LINKS;
 
 my $js = JSON::XS->new->pretty->canonical;
 