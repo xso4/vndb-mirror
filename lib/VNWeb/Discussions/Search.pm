xso4/vndb-mirror

commit dd7cf35afffa2fd911bf1190b10408a31be5344b

FU: Convert discussion-related queries

lib/VNWeb/Discussions/Search.pm
@@ -16,7 +16,7 @@ sub filters_ {
     );
     my %boards = map +($_,1), $filt->{b}->@*;
 
-    my $u = $filt->{uq} && fu->dbVali('SELECT id FROM users WHERE', $filt->{uq} =~ /^u$RE{num}$/ ? 'id = ' : 'lower(username) =', \lc $filt->{uq});
+    my $u = $filt->{uq} && fu->SQL('SELECT id FROM users WHERE', $filt->{uq} =~ /^u$RE{num}$/ ? 'id = ' : 'lower(username) =', lc $filt->{uq})->val;
 
     form_ method => 'get', action => fu->path, sub {
         boardtypes_;
@@ -56,14 +56,13 @@ sub noresults_ {
 }
 
 
-sub posts_ {
-    my($filt, $u) = @_;
-
+sub posts_($filt, $u) {
     # Use websearch_to_tsquery() to convert the query string into a tsquery.
     # Also match against an empty string to see if the query doesn't consist of only negative matches.
-    my $ts = fu->dbVali('
-        WITH q(q) AS (SELECT websearch_to_tsquery(', \$filt->{bq}, '))
-        SELECT CASE WHEN numnode(q) = 0 OR q @@ \'\' THEN NULL ELSE q::text END FROM q');
+    my $ts = fu->sql(q{
+        WITH q(q) AS (SELECT websearch_to_tsquery($1))
+        SELECT CASE WHEN numnode(q) = 0 OR q @@ '' THEN NULL ELSE q::text END FROM q
+    }, $filt->{bq})->val;
     return noresults_ if !$ts;
 
     my $reviews = grep $_ eq 'w', $filt->{b}->@*;
@@ -75,38 +74,39 @@ sub posts_ {
     # means we can re-use them for highlighting without worrying that they
     # conflict with the message contents.
 
-    my($posts, $np) = fu->dbPagei({ results => 20, page => $filt->{p} }, q{
-        SELECT m.id, m.num, m.title
-             , }, sql_user(), q{
-             , }, sql_totime('m.date'), q{as date
-             , ts_headline('english', strip_bb_tags(strip_spoilers(m.msg)),}, \$ts, ',',
-                 \'MaxFragments=2,MinWords=15,MaxWords=40,StartSel=[raw],StopSel=[/raw],FragmentDelimiter=[code]',
-               ') as headline
-          FROM (', sql_join('UNION',
+    $ts = SQL($ts, '::text::tsquery');
+    my $posts = fu->SQL('
+        SELECT m.id, m.num, m.title,', USER, ", m.date
+             , ts_headline('english', strip_bb_tags(strip_spoilers(m.msg)),", $ts, ",
+                 'MaxFragments=2,MinWords=15,MaxWords=40,StartSel=[raw],StopSel=[/raw],FragmentDelimiter=[code]'
+               ) as headline
+          FROM (", INTERSPERSE('UNION',
              @tboards ?
-                sql('SELECT tp.tid, tp.num, t.title, tp.uid, tp.date, tp.msg
+                SQL('SELECT tp.tid, tp.num, t.title, tp.uid, tp.date, tp.msg
                        FROM threads_posts tp
                        JOIN threads t ON t.id = tp.tid
                       WHERE NOT t.hidden AND NOT t.private AND tp.hidden IS NULL
-                        AND bb_tsvector(tp.msg) @@', \$ts,
-                            $u ? ('AND tp.uid =', \$u) : (),
-                            @tboards < keys %BOARD_TYPE ? ('AND t.id IN(SELECT tid FROM threads_boards WHERE type IN', \@tboards, ')') : ()
+                        AND bb_tsvector(tp.msg) @@', $ts,
+                            $u ? ('AND tp.uid =', $u) : (),
+                            @tboards < keys %BOARD_TYPE ? ('AND t.id IN(SELECT tid FROM threads_boards WHERE type', IN \@tboards, ')') : ()
              ) : (), $reviews ? (
-                 sql('SELECT w.id, 0, v.title[1+1], w.uid, w.date, w.text
+                 SQL('SELECT w.id, 0, v.title[1+1], w.uid, w.date, w.text
                         FROM reviews w
-                        JOIN', vnt, 'v ON v.id = w.vid
-                       WHERE NOT w.c_flagged AND bb_tsvector(w.text) @@', \$ts,
-                             $u ? ('AND w.uid =', \$u) : ()),
-                 sql('SELECT wp.id, wp.num, v.title[1+1], wp.uid, wp.date, wp.msg
+                        JOIN', VNT, 'v ON v.id = w.vid
+                       WHERE NOT w.c_flagged AND bb_tsvector(w.text) @@', $ts,
+                             $u ? ('AND w.uid =', $u) : ()),
+                 SQL('SELECT wp.id, wp.num, v.title[1+1], wp.uid, wp.date, wp.msg
                         FROM reviews_posts wp
                         JOIN reviews w ON w.id = wp.id
-                        JOIN', vnt, 'v ON v.id = w.vid
-                       WHERE NOT w.c_flagged AND wp.hidden IS NULL AND bb_tsvector(wp.msg) @@', \$ts,
-                             $u ? ('AND wp.uid =', \$u) : ()),
+                        JOIN', VNT, 'v ON v.id = w.vid
+                       WHERE NOT w.c_flagged AND wp.hidden IS NULL AND bb_tsvector(wp.msg) @@', $ts,
+                             $u ? ('AND wp.uid =', $u) : ()),
              ) : ()), ') m (id, num, title, uid, date, msg)
           LEFT JOIN users u ON u.id = m.uid
-         ORDER BY m.date DESC'
-    );
+         ORDER BY m.date DESC
+         LIMIT 20 OFFSET', 20*($filt->{p}-1)+1
+    )->allh;
+    my $np = @$posts > 20 && !!pop @$posts;
 
     return noresults_ if !@$posts;
 
@@ -144,16 +144,14 @@ sub posts_ {
 }
 
 
-sub threads_ {
-    my($filt, $u) = @_;
-
+sub threads_($filt, $u) {
     my @boards = grep $_ ne 'w', $filt->{b}->@*; # Can't search reviews by title
     return noresults_ if !@boards;
 
-    my $where = sql_and
-        @boards < keys %BOARD_TYPE ? sql('t.id IN(SELECT tid FROM threads_boards WHERE type IN', \@boards, ')') : (),
-        $u ? sql('EXISTS(SELECT 1 FROM threads_posts tp WHERE tp.tid = t.id AND tp.num = 1 AND tp.uid =', \$u, ')') : (),
-        map sql('t.title ilike', \('%'.sql_like($_).'%')), grep length($_) > 0, split /[ ,._-]/, $filt->{bq};
+    my $where = AND
+        @boards < keys %BOARD_TYPE ? SQL('t.id IN(SELECT tid FROM threads_boards WHERE type', IN \@boards, ')') : (),
+        $u ? SQL('EXISTS(SELECT 1 FROM threads_posts tp WHERE tp.tid = t.id AND tp.num = 1 AND tp.uid =', $u, ')') : (),
+        map SQL('t.title ilike', '%'.sql_like($_).'%'), grep length($_) > 0, split /[ ,._-]/, $filt->{bq};
 
     noresults_ if !threadlist_
         where    => $where,

lib/VNWeb/Discussions/Board.pm
@@ -30,8 +30,8 @@ FU::get qr{/t/(all|$BOARD_RE)}, sub($id) {
         };
 
         threadlist_
-            where    => $type ne 'all' && sql('t.id IN(SELECT tid FROM threads_boards WHERE type =', \$type, $id ? ('AND iid =', \$id) : (), ')'),
-            boards   => $type ne 'all' && sql('NOT (tb.type =', \$type, 'AND tb.iid IS NOT DISTINCT FROM', \$id, ')'),
+            where    => $type ne 'all' && SQL('t.id IN(SELECT tid FROM threads_boards WHERE type =', $type, $id ? ('AND iid =', $id) : (), ')'),
+            boards   => $type ne 'all' && SQL('NOT (tb.type =', $type, 'AND tb.iid IS NOT DISTINCT FROM', $id, ')'),
             results  => 50,
             sort     => $type eq 'an' ? 't.id DESC' : undef,
             page     => $page,

lib/VNWeb/Discussions/Edit.pm
@@ -32,11 +32,10 @@ my $FORM = form_compile {
 
 js_api DiscussionDelete => { id => { vndbid => 't' } }, sub ($data) {
     fu->denied if !auth->permBoardmod;
-    my $uid = fu->dbVali('SELECT uid FROM threads_posts WHERE num = 1 AND tid =', \$data->{id});
-    fu->notfound if !$uid;
+    my $uid = fu->sql('SELECT uid FROM threads_posts WHERE num = 1 AND tid = $1', $data->{id})->val or fu->notfound;
     auth->audit($uid, 'post delete', "deleted $data->{id}.1");
-    fu->dbExeci('DELETE FROM notifications WHERE iid =', \$data->{id});
-    fu->dbExeci('DELETE FROM threads WHERE id =', \$data->{id});
+    fu->sql('DELETE FROM notifications WHERE iid = $1', $data->{id})->exec;
+    fu->sql('DELETE FROM threads WHERE id = $1', $data->{id})->exec;
     return +{ _redir => '/t' };
 };
 
@@ -44,16 +43,16 @@ js_api DiscussionDelete => { id => { vndbid => 't' } }, sub ($data) {
 js_api DiscussionEdit => $FORM, sub ($data) {
     my $tid = $data->{tid};
 
-    my $t = !$tid ? {} : fu->dbRowi('
-        SELECT t.id, t.poll_question, t.poll_max_options, t.boards_locked, t.hidden, tp.num, tp.uid AS user_id,', sql_totime('tp.date'), 'AS date
+    my $t = !$tid ? {} : fu->SQL('
+        SELECT t.id, t.poll_question, t.poll_max_options, t.boards_locked, t.hidden, tp.num, tp.uid AS user_id, tp.date
           FROM threads t
           JOIN threads_posts tp ON tp.tid = t.id AND tp.num = 1
-         WHERE t.id =', \$tid,
-          'AND', sql_visible_threads());
+         WHERE t.id =', $tid, 'AND', VISIBLE_THREADS
+    )->rowh;
     fu->notfound if $tid && !$t->{id};
     fu->denied if !can_edit t => $t;
 
-    fu->dbExeci('DELETE FROM notifications WHERE iid =', \$tid) if $tid && auth->permBoardmod && $data->{hidden};
+    fu->sql('DELETE FROM notifications WHERE iid = $1', $tid)->exec if $tid && auth->permBoardmod && $data->{hidden};
     auth->audit($t->{user_id}, 'post edit', "edited $tid.1") if $tid && $t->{user_id} ne auth->uid;
 
     return 'Invalid boards' if !$data->{boards} || grep +(!$BOARD_TYPE{$_->{btype}}{dbitem})^(!$_->{iid}), $data->{boards}->@*;
@@ -67,7 +66,7 @@ js_api DiscussionEdit => $FORM, sub ($data) {
              $data->{poll}{question} ne ($t->{poll_question}||'')
           || $data->{poll}{max_options} != $t->{poll_max_options}
           || join("\n", $data->{poll}{options}->@*) ne
-             join("\n", map $_->{option}, fu->dbAlli('SELECT option FROM threads_poll_options WHERE tid =', \$tid, 'ORDER BY id')->@*)
+             join("\n", fu->SQL('SELECT option FROM threads_poll_options WHERE tid =', $tid, 'ORDER BY id')->flat->@*)
     ));
 
     my $thread = {
@@ -83,17 +82,17 @@ js_api DiscussionEdit => $FORM, sub ($data) {
             private => $data->{private}
         ) : (),
     };
-    fu->dbExeci('UPDATE threads SET', $thread, 'WHERE id =', \$tid) if $tid;
-    $tid = fu->dbVali('INSERT INTO threads', $thread, 'RETURNING id') if !$tid;
+    fu->SQL('UPDATE threads', SET($thread), 'WHERE id =', $tid)->exec if $tid;
+    $tid = fu->SQL('INSERT INTO threads', VALUES($thread), 'RETURNING id')->val if !$tid;
 
     if(auth->permBoardmod || !$t->{boards_locked}) {
-        fu->dbExeci('DELETE FROM threads_boards WHERE tid =', \$tid);
-        fu->dbExeci('INSERT INTO threads_boards', { tid => $tid, type => $_->{btype}, iid => $_->{iid} }) for $data->{boards}->@*;
+        fu->SQL('DELETE FROM threads_boards WHERE tid =', $tid)->exec;
+        fu->SQL('INSERT INTO threads_boards', VALUES { tid => $tid, type => $_->{btype}, iid => $_->{iid} })->exec for $data->{boards}->@*;
     }
 
     if($pollchanged) {
-        fu->dbExeci('DELETE FROM threads_poll_options WHERE tid =', \$tid);
-        fu->dbExeci('INSERT INTO threads_poll_options', { tid => $tid, option => $_ }) for $data->{poll}{options}->@*;
+        fu->SQL('DELETE FROM threads_poll_options WHERE tid =', $tid)->exec;
+        fu->SQL('INSERT INTO threads_poll_options', VALUES { tid => $tid, option => $_ })->exec for $data->{poll}{options}->@*;
     }
 
     my $post = {
@@ -101,10 +100,10 @@ js_api DiscussionEdit => $FORM, sub ($data) {
         num => 1,
         msg => bb_subst_links($data->{msg}),
         $data->{tid} ? () : (uid => auth->uid),
-        !$data->{tid} || (auth->permBoardmod && $data->{nolastmod}) ? () : (edited => sql 'NOW()')
+        !$data->{tid} || (auth->permBoardmod && $data->{nolastmod}) ? () : (edited => SQL 'NOW()')
     };
-    fu->dbExeci('INSERT INTO threads_posts', $post) if !$data->{tid};
-    fu->dbExeci('UPDATE threads_posts SET', $post, 'WHERE', { tid => $tid, num => 1 }) if $data->{tid};
+    fu->SQL('INSERT INTO threads_posts', VALUES $post)->exec if !$data->{tid};
+    fu->SQL('UPDATE threads_posts', SET($post), WHERE { tid => $tid, num => 1 })->exec if $data->{tid};
 
     +{ _redir => "/$tid.1" };
 };
@@ -119,22 +118,22 @@ FU::get qr{(?:/t/($BOARD_RE)/new|/$RE{tid}/edit)}, sub($board_id,$tid=undef) {
 
     $board_type = 'ge' if $board_type && $board_type eq 'an' && !auth->permBoardmod;
 
-    my $t = !$tid ? {} : fu->dbRowi('
-        SELECT t.id, tp.tid, t.title, t.locked, t.boards_locked, t.private, t.hidden, t.poll_question, t.poll_max_options, tp.msg, tp.uid AS user_id,', sql_totime('tp.date'), 'AS date
+    my $t = !$tid ? {} : fu->SQL('
+        SELECT t.id, tp.tid, t.title, t.locked, t.boards_locked, t.private, t.hidden, t.poll_question, t.poll_max_options, tp.msg, tp.uid AS user_id, tp.date
           FROM threads t
           JOIN threads_posts tp ON tp.tid = t.id AND tp.num = 1
-         WHERE t.id =', \$tid,
-          'AND', sql_visible_threads());
+         WHERE t.id =', $tid, 'AND', VISIBLE_THREADS
+    )->rowh;
     fu->notfound if $tid && !$t->{id};
     fu->denied if !can_edit t => $t;
 
-    $t->{poll}{options} = $t->{poll_question} && [ map $_->{option}, fu->dbAlli('SELECT option FROM threads_poll_options WHERE tid =', \$t->{id}, 'ORDER BY id')->@* ];
+    $t->{poll}{options} = $t->{poll_question} && fu->SQL('SELECT option FROM threads_poll_options WHERE tid =', $t->{id}, 'ORDER BY id')->flat;
     $t->{poll}{question} = delete $t->{poll_question};
     $t->{poll}{max_options} = delete $t->{poll_max_options};
     $t->{poll} = undef if !$t->{poll}{question};
 
     if($tid) {
-        enrich_boards undef, $t;
+        enrich_boards undef, [$t];
     } else {
         $t->{boards} = [ {
             id    => $board_id ? $board_id->{id} : $board_type,

lib/VNWeb/Discussions/Index.pm
@@ -25,8 +25,8 @@ FU::get '/t', sub {
                 };
             };
             threadlist_
-                where   => sql('t.id IN(SELECT tid FROM threads_boards WHERE type =', \$b, ')'),
-                boards  => sql('NOT (tb.type =', \$b, 'AND tb.iid IS NULL)'),
+                where   => SQL('t.id IN(SELECT tid FROM threads_boards WHERE type =', $b, ')'),
+                boards  => SQL('NOT (tb.type =', $b, 'AND tb.iid IS NULL)'),
                 results => $BOARD_TYPE{$b}{index_rows},
                 page    => 1;
         }

lib/VNWeb/Discussions/Lib.pm
@@ -3,30 +3,28 @@ package VNWeb::Discussions::Lib;
 use VNWeb::Prelude;
 use Exporter 'import';
 
-our @EXPORT = qw/$BOARD_RE sql_visible_threads enrich_boards threadlist_ boardsearch_ boardtypes_/;
+our @EXPORT = qw/$BOARD_RE VISIBLE_THREADS enrich_boards threadlist_ boardsearch_ boardtypes_/;
 
 
 our $BOARD_RE = join '|', map $_.($BOARD_TYPE{$_}{dbitem}?'(?:[1-9][0-9]{0,5})?':''), keys %BOARD_TYPE;
 
 
 # Returns a WHERE condition to filter threads that the current user is allowed to see.
-sub sql_visible_threads {
-    return '1=1' if auth && auth->uid eq 'u2'; # Yorhel sees everything
-    sql_and
-        auth->permBoardmod ? () : ('NOT t.hidden'),
-        sql('NOT t.private OR EXISTS(SELECT 1 FROM threads_boards WHERE tid = t.id AND type = \'u\' AND iid =', \auth->uid, ')');
+sub VISIBLE_THREADS {
+    return RAW 'true' if auth && auth->uid eq 'u2'; # Yorhel sees everything
+    AND auth->permBoardmod ? () : ('NOT t.hidden'),
+        SQL("NOT t.private OR EXISTS(SELECT 1 FROM threads_boards WHERE tid = t.id AND type = 'u' AND iid =", auth->uid, ')');
 }
 
 
 # Adds a 'boards' array to threads.
-sub enrich_boards {
-    my($filt, @lst) = @_;
-    enrich boards => id => tid => sub { sql '
+sub enrich_boards($filt, $lst) {
+    fu->enrich(aoh => boards => sub { SQL '
         SELECT tb.tid, COALESCE(tb.iid::text, tb.type::text) AS id, tb.type AS btype, tb.iid, x.title
-          FROM threads_boards tb, ', item_info('tb.iid', 'NULL'), 'x
-         WHERE ', sql_and(sql('tb.tid IN', $_[0]), $filt||()), '
+          FROM threads_boards tb, ', ITEM_INFO('tb.iid', 'NULL'), 'x
+         WHERE ', AND(SQL('tb.tid', IN $_), $filt||()), '
          ORDER BY tb.type, tb.iid
-    '}, @lst;
+    '}, $lst);
 }
 
 
@@ -40,23 +38,24 @@ sub enrich_boards {
 #
 # Returns 1 if something was displayed, 0 if no threads matched the where clause.
 sub threadlist_(%opt) {
-    my $where = sql_and sql_visible_threads(), $opt{where}||();
+    my $where = AND VISIBLE_THREADS, $opt{where}||();
 
-    my $count = $opt{paginate} && fu->dbVali('SELECT count(*) FROM threads t WHERE', $where);
+    my $count = $opt{paginate} && fu->SQL('SELECT count(*) FROM threads t WHERE', $where)->val;
     return 0 if $opt{paginate} && !$count;
 
-    my $lst = fu->dbPagei(\%opt, q{
+    my $lst = fu->SQL('
         SELECT t.id, t.title, t.c_count, t.c_lastnum, t.locked, t.private, t.hidden, t.poll_question IS NOT NULL AS haspoll
-             , }, sql_user('tfu', 'firstpost_'), ',', sql_totime('tf.date'), q{ as firstpost_date
-             , }, sql_user('tlu', 'lastpost_'),  ',', sql_totime('tl.date'), q{ as lastpost_date
+             , ', USER('tfu', 'firstpost_'), ', tf.date as firstpost_date
+             , ', USER('tlu', 'lastpost_'),  ', tl.date as lastpost_date
           FROM threads t
           JOIN threads_posts tf ON tf.tid = t.id AND tf.num = 1
           JOIN threads_posts tl ON tl.tid = t.id AND tl.num = t.c_lastnum
           LEFT JOIN users tfu ON tfu.id = tf.uid
           LEFT JOIN users tlu ON tlu.id = tl.uid
-         WHERE }, $where, q{
-         ORDER BY}, $opt{sort}||'tl.date DESC'
-    );
+         WHERE ', $where, '
+         ORDER BY', RAW($opt{sort}||'tl.date DESC'), '
+         LIMIT', $opt{results}, OFFSET => $opt{results}*($opt{page}-1)
+    )->allh;
     return 0 if !@$lst;
 
     enrich_boards $opt{boards}, $lst;

lib/VNWeb/Discussions/PostEdit.pm
@@ -19,15 +19,15 @@ my($FORM_IN, $FORM_OUT) = form_compile 'in', 'out', {
 
 
 sub _info($id, $num) {
-    fu->dbRowi('
-        SELECT t.id, tp.num, tp.hidden, tp.msg, tp.uid AS user_id,', sql_totime('tp.date'), 'AS date
+    fu->SQL('
+        SELECT t.id, tp.num, tp.hidden, tp.msg, tp.uid AS user_id, tp.date
           FROM threads t
-          JOIN threads_posts tp ON tp.tid = t.id AND tp.num =', \$num, '
-         WHERE t.id =', \$id, 'AND', sql_visible_threads(),'
+          JOIN threads_posts tp ON tp.tid = t.id AND tp.num =', $num, '
+         WHERE t.id =', $id, 'AND', VISIBLE_THREADS, '
        UNION ALL
-        SELECT id, num, hidden, msg, uid AS user_id,', sql_totime('date'), 'AS date
-          FROM reviews_posts WHERE id =', \$id, 'AND num =', \$num
-    );
+        SELECT id, num, hidden, msg, uid AS user_id, date
+          FROM reviews_posts WHERE id =', $id, 'AND num =', $num
+    )->rowh;
 }
 
 
@@ -35,16 +35,15 @@ js_api PostEdit => $FORM_IN, sub ($data) {
     my $id  = $data->{id};
     my $num = $data->{num};
 
-    my $t = _info $id, $num;
-    fu->notfound if !$t->{id};
+    my $t = _info $id, $num or fu->notfound;
     fu->denied if !can_edit t => $t;
 
-    fu->dbExeci(q{DELETE FROM notifications WHERE iid =}, \$id, 'AND num =', \$num) if auth->permBoardmod && ($data->{delete} || defined $data->{hidden});
+    fu->SQL('DELETE FROM notifications WHERE iid =', $id, 'AND num =', $num)->exec if auth->permBoardmod && ($data->{delete} || defined $data->{hidden});
 
     if($data->{delete} && auth->permBoardmod) {
         auth->audit($t->{user_id}, 'post delete', "deleted $id.$num");
-        fu->dbExeci('DELETE FROM threads_posts WHERE tid =', \$id, 'AND num =', \$num);
-        fu->dbExeci('DELETE FROM reviews_posts WHERE  id =', \$id, 'AND num =', \$num);
+        fu->SQL('DELETE FROM threads_posts WHERE tid =', $id, 'AND num =', $num)->exec;
+        fu->SQL('DELETE FROM reviews_posts WHERE  id =', $id, 'AND num =', $num)->exec;
         return +{ _redir => "/$id" };
     }
     auth->audit($t->{user_id}, 'post edit', "edited $id.$num") if $t->{user_id} ne auth->uid;
@@ -54,11 +53,11 @@ js_api PostEdit => $FORM_IN, sub ($data) {
         num => $num,
         msg => bb_subst_links($data->{msg}),
         auth->permBoardmod ? (hidden => $data->{hidden}) : (),
-        (auth->permBoardmod && $data->{nolastmod}) ? () : (edited => sql 'NOW()')
+        (auth->permBoardmod && $data->{nolastmod}) ? () : (edited => SQL 'NOW()')
     };
-    fu->dbExeci('UPDATE threads_posts SET', $post, 'WHERE', { tid => $id, num => $num }) if $id =~ /^t/;
+    fu->SQL('UPDATE threads_posts', SET($post), WHERE { tid => $id, num => $num })->exec if $id =~ /^t/;
     $post->{id} = delete $post->{tid};
-    fu->dbExeci('UPDATE reviews_posts SET', $post, 'WHERE', {  id => $id, num => $num }) if $id =~ /^w/;
+    fu->SQL('UPDATE reviews_posts', SET($post), WHERE {  id => $id, num => $num })->exec if $id =~ /^w/;
 
     +{ _redir => "/$id.$num" };
 };
@@ -66,10 +65,7 @@ js_api PostEdit => $FORM_IN, sub ($data) {
 
 FU::get qr{/(?:$RE{tid}|$RE{wid})\.($RE{num})/edit}, sub($tid, $wid, $num) {
     fu->redirect(temp => "/$tid/edit") if $tid && $num == 1;
-    my $id = $tid || $wid;
-
-    my $t = _info $id, $num;
-    fu->notfound if $id && !$t->{id};
+    my $t = _info $tid || $wid, $num or fu->notfound;
     fu->denied if !can_edit t => $t;
 
     $t->{can_mod} = auth->permBoardmod;

lib/VNWeb/Discussions/Thread.pm
@@ -11,15 +11,18 @@ my $REPLY = form_compile {
     msg => { maxlength => 32768 }
 };
 
-js_api DiscussionReply => $REPLY, sub {
-    my($data) = @_;
-    my $t = fu->dbRowi('SELECT id, locked FROM threads t WHERE id =', \$data->{tid}, 'AND', sql_visible_threads());
-    fu->notfound if !$t->{id};
+js_api DiscussionReply => $REPLY, sub($data) {
+    my $t = fu->SQL(
+        'SELECT id, locked FROM threads t WHERE id =', $data->{tid}, 'AND', VISIBLE_THREADS
+    )->rowh or fu->notfound;
     fu->denied if !can_edit t => $t;
 
-    my $num = sql '(SELECT MAX(num)+1 FROM threads_posts WHERE tid =', \$data->{tid}, ')';
-    my $msg = bb_subst_links $data->{msg};
-    $num = fu->dbVali('INSERT INTO threads_posts', { tid => $t->{id}, num => $num, uid => auth->uid, msg => $msg }, 'RETURNING num');
+    my $num = fu->SQL('INSERT INTO threads_posts', VALUES({
+        tid => $t->{id},
+        num => SQL('(SELECT MAX(num)+1 FROM threads_posts WHERE tid =', $data->{tid}, ')'),
+        uid => auth->uid,
+        msg => bb_subst_links($data->{msg}),
+    }), 'RETURNING num')->val;
     +{ _redir => "/$t->{id}.$num#last" };
 };
 
@@ -59,23 +62,23 @@ sub metabox_ {
 
 
 sub poll_ ($t) {
-    my $options = fu->dbAlli(
+    my $options = fu->SQL(
         'SELECT tpo.id, tpo.option, count(u.id) as votes, tpm.optid IS NOT NULL as my
            FROM threads_poll_options tpo
            LEFT JOIN threads_poll_votes tpv ON tpv.optid = tpo.id
            LEFT JOIN users u ON tpv.uid = u.id AND NOT u.ign_votes
-           LEFT JOIN threads_poll_votes tpm ON tpm.optid = tpo.id AND tpm.uid =', \auth->uid, '
-          WHERE tpo.tid =', \$t->{id}, '
+           LEFT JOIN threads_poll_votes tpm ON tpm.optid = tpo.id AND tpm.uid =', auth->uid, '
+          WHERE tpo.tid =', $t->{id}, '
           GROUP BY tpo.id, tpo.option, tpm.optid
           ORDER BY tpo.id'
-    );
-    my $num_votes = fu->dbVali(
+    )->allh;
+    my $num_votes = fu->SQL(
         'SELECT COUNT(DISTINCT tpv.uid)
           FROM threads_poll_votes tpv
           JOIN threads_poll_options tpo ON tpo.id = tpv.optid
           JOIN users u ON tpv.uid = u.id
-         WHERE NOT u.ign_votes AND tpo.tid =', \$t->{id}
-    );
+         WHERE NOT u.ign_votes AND tpo.tid =', $t->{id}
+    )->val;
     my $preview = $num_votes && (fu->query(pollview => { anybool => 1 }) || !auth || grep $_->{my}, @$options);
     my $max_votes = max map $_->{votes}, @$options;
 
@@ -120,9 +123,9 @@ sub posts_ {
     my($t, $posts, $page) = @_;
     my sub url { "/$t->{id}".($_?"/$_":'') }
 
-    enrich patrolled => num => num =>
-        sql('SELECT p.num,', sql_user(), 'FROM posts_patrolled p JOIN users u ON u.id = p.uid WHERE p.id =', \$t->{id}, 'AND p.num IN'), $posts
-        if auth->permDbmod;
+    fu->enrich(key => 'num', aoh => 'patrolled',
+        SQL('SELECT p.num,', USER, 'FROM posts_patrolled p JOIN users u ON u.id = p.uid WHERE p.id =', $t->{id}, 'AND p.num'), $posts
+    ) if auth->permDbmod;
 
     paginate_ \&url, $page, [ $t->{count}, 25 ], 't';
     article_ class => 'thread', id => 'threadstart', sub {
@@ -174,8 +177,8 @@ sub posts_ {
 sub mark_patrolled($id, $num) {
     return if !auth->permDbmod;
     my $obj = { id => $id, num => $num, uid => auth->uid };
-    fu->dbExeci('INSERT INTO posts_patrolled', $obj, 'ON CONFLICT (id,num,uid) DO NOTHING') if fu->query('patrolled');
-    fu->dbExeci('DELETE FROM posts_patrolled WHERE', $obj) if fu->query('unpatrolled');
+    fu->SQL('INSERT INTO posts_patrolled', VALUES($obj), 'ON CONFLICT (id,num,uid) DO NOTHING')->exec if fu->query('patrolled');
+    fu->SQL('DELETE FROM posts_patrolled', WHERE $obj)->exec if fu->query('unpatrolled');
 }
 
 
@@ -201,31 +204,28 @@ FU::get $PATH, sub($id, $sep='', $num=0) {
     not_moe;
     mark_patrolled $id, $num if $sep eq '.';
 
-    my $t = fu->dbRowi(
+    my $t = fu->SQL(
         'SELECT id, title, hidden, locked, private
               , poll_question, poll_max_options
               , (SELECT COUNT(*) FROM threads_posts WHERE tid = id) AS count
            FROM threads t
-          WHERE', sql_visible_threads(), 'AND id =', \$id
-    );
-    fu->notfound if !$t->{id};
+          WHERE', VISIBLE_THREADS, 'AND id =', $id
+    )->rowh or fu->notfound;
 
-    enrich_boards '', $t;
+    enrich_boards '', [$t];
 
     my $page = $sep eq '/' ? $num||1 : $sep ne '.' ? 1
-        : ceil((fu->dbVali('SELECT COUNT(*) FROM threads_posts WHERE num <=', \$num, 'AND tid =', \$id)||9999)/25);
+        : ceil((fu->SQL('SELECT COUNT(*) FROM threads_posts WHERE num <=', $num, 'AND tid =', $id)->val||9999)/25);
     $num = 0 if $sep ne '.';
 
-    my $posts = fu->dbPagei({ results => 25, page => $page },
-        'SELECT tp.tid as id, tp.num, tp.hidden, tp.msg',
-             ',', sql_user(),
-             ',', sql_totime('tp.date'), ' as date',
-             ',', sql_totime('tp.edited'), ' as edited
+    my $posts = fu->SQL(
+        'SELECT tp.tid as id, tp.num, tp.hidden, tp.msg,', USER, ', tp.date, tp.edited
            FROM threads_posts tp
            LEFT JOIN users u ON tp.uid = u.id
-          WHERE tp.tid =', \$id, '
-          ORDER BY tp.num'
-    );
+          WHERE tp.tid =', $id, '
+          ORDER BY tp.num
+          LIMIT 25 OFFSET', 25*($page-1)
+    )->allh;
     fu->notfound if !@$posts || ($num && !grep $_->{num} == $num, @$posts);
 
     auth->notiRead($id, [ map $_->{num}, $posts->@* ]) if @$posts;
@@ -242,16 +242,17 @@ FU::get $PATH, sub($id, $sep='', $num=0) {
 FU::post $PATH, sub($id, @) {
     fu->denied if !auth || !auth->csrfcheck(fu->formdata(csrf => { onerror => '' }), "poll-$id");
 
-    my $t = fu->dbRowi('SELECT poll_question, poll_max_options FROM threads t WHERE id =', \$id, 'AND', sql_visible_threads());
-    fu->notfound if !$t->{poll_question};
+    my $t = fu->SQL(
+        'SELECT poll_question, poll_max_options FROM threads t WHERE id =', $id, 'AND', VISIBLE_THREADS
+    )->rowh || fu->notfound;
 
-    my %opt = map +($_->{id},1), fu->dbAlli('SELECT id FROM threads_poll_options WHERE tid =', \$id)->@*;
-    my %vote = map +($_,1), grep $opt{$_}, fu->formdata(opt => { default => [], accept_scalar => 1, elems => { uint => 1 } })->@*;
+    my $opt = fu->sql('SELECT id FROM threads_poll_options WHERE tid = $1', $id)->kvv;
+    my %vote = map +($_,1), grep $opt->{$_}, fu->formdata(opt => { default => [], accept_scalar => 1, elems => { uint => 1 } })->@*;
     my $i = 0;
     my @vote = grep $i++ < $t->{poll_max_options}, sort keys %vote;
 
-    fu->dbExeci('DELETE FROM threads_poll_votes WHERE optid IN', [ keys %opt ], 'AND uid =', \auth->uid);
-    fu->dbExeci('INSERT INTO threads_poll_votes', { uid => auth->uid, optid => $_ }) for @vote;
+    fu->SQL('DELETE FROM threads_poll_votes WHERE optid', IN [ keys %$opt ], 'AND uid =', auth->uid)->exec;
+    fu->SQL('INSERT INTO threads_poll_votes', VALUES { uid => auth->uid, optid => $_ })->exec for @vote;
     fu->redirect(tempget => fu->path);
 };
 

commit 6b8ac9ca720a8211999c07a56bd22323db78d5ae

Discussions::Search: Fix pagination
Broken in dd7cf35afffa2fd911bf1190b10408a31be5344b

lib/VNWeb/Discussions/Search.pm
@@ -104,7 +104,7 @@ sub posts_($filt, $u) {
              ) : ()), ') m (id, num, title, uid, date, msg)
           LEFT JOIN users u ON u.id = m.uid
          ORDER BY m.date DESC
-         LIMIT 20 OFFSET', 20*($filt->{p}-1)+1
+         LIMIT 21 OFFSET', 20*($filt->{p}-1)
     )->allh;
     my $np = @$posts > 20 && !!pop @$posts;
 