xso4/vndb-mirror

commit 4cae2dcc55d232c4219c5365e645d7f904b2c700

VNLength: Add language field

js/basic/ds.js
@@ -111,6 +111,7 @@ class DS {
         this.source = source;
         if (source && source.opts) Object.assign(this, source.opts);
         if (opts) Object.assign(this, opts);
+        if (!('keep' in this)) this.keep = this.checked;
         this.open = this.open.bind(this);
         this.list = [];
     }
@@ -132,12 +133,12 @@ class DS {
     select() {
         const obj = this.list.find(e => e.id === this.selId);
         if (!obj) return;
-        if (this.checked || this.keep) this.focus = v => { this.focus = null; v.dom.focus() };
+        if (this.keep) this.focus = v => { this.focus = null; v.dom.focus() };
         this.input_override = false; // honor setInput() in onselect()
         if (this.autocomplete) this.autocomplete(this.source.stringify ? this.source.stringify(obj) : obj.id);
         else if (this.onselect) this.onselect(obj, !this.checked || !this.checked(obj));
+        if (!this.keep) close();
         if (!this.checked) {
-            if (!this.keep) close();
             if (!this.input_override) this.setInput('');
             this.selId = null;
         }
@@ -176,7 +177,7 @@ class DS {
         } else if (ev.key == 'Tab') {
             const f = this.list.find(e => e.id === this.selId);
             ev.shiftKey || !f ? close() : this.select();
-            if (this.keep || this.checked) close(); // Tab always closes, even on multiselection boxes
+            if (this.keep) close(); // Tab always closes, even on multiselection boxes
             if (!this.autocomplete) ev.preventDefault();
         }
     }

js/user/VNLengthVote.js
@@ -10,10 +10,22 @@ widget('VNLengthVote', initVnode => {
         notes: '',
     };
 
+    const langs = Object.fromEntries(vndbTypes.language);
+    const availableLangs = () => new Set(data.releases.flatMap(r => vote.rid.includes(r.id) ? r.lang : []));
+    const setlang = () => {
+        const l = availableLangs();
+        if (l.size === 1) vote.lang = [availableLangs().values().next().value];
+        if (!vote.lang) vote.lang = [];
+        vote.lang = vote.lang.filter(v => l.has(v));
+    };
+    setlang();
+
     const relDs = data.releases.length === 1 && vote.rid.length === 1 && vote.rid[0] === data.releases[0].id ? null : new DS(DS.Releases(data.releases), {
+        keep: false,
         onselect: (obj,c) => {
             if (c && !vote.rid.includes(obj.id)) vote.rid.push(obj.id);
             else vote.rid = vote.rid.filter(x => x !== obj.id);
+            setlang();
         },
         checked: obj => vote.rid.includes(obj.id),
     });
@@ -40,12 +52,23 @@ widget('VNLengthVote', initVnode => {
                     m('label[for=rel]', 'Release(s)'),
                     vote.rid.length === 0 ? m('div', 'Which release did you play?') : null,
                     vote.rid.map(id => (r => m('div', {key: id},
-                        relDs ? m(Button.Del, { onclick: () => vote.rid = vote.rid.filter(x => x !== id) }) : null,
+                        relDs ? m(Button.Del, { onclick: () => setlang(vote.rid = vote.rid.filter(x => x !== id)) }) : null,
                         ' ',
                         r ? Release(r,1) : 'Moved or deleted release: '+id
                     ))(data.releases.find(r => r.id == id))),
                     relDs ? m(DS.Button, { ds: relDs, id: 'rel' }, 'Select release') : null,
                     vote.rid.length === 0 ? m('p.invalid', 'Please select a release') : null,
+                ),
+                vote.rid.length === 0 ? null : m('fieldset',
+                    m('label', 'Language(s)'),
+                    availableLangs().size === 1 ? [LangIcon(vote.lang[0]), langs[vote.lang[0]]] :
+                        [...availableLangs().values()].map(l => m('label.check',
+                            m('input[type=checkbox]', { checked: vote.lang.includes(l), onclick: ev => {
+                                if (ev.target.checked) vote.lang.push(l);
+                                else vote.lang = vote.lang.filter(x => x !== l);
+                            }}), ' ', LangIcon(l), langs[l]
+                        )).intersperse(m('small', ' / ')),
+                    vote.lang.length === 0 ? m('p.invalid', 'No language selected.') : null,
                 ),
             ), m('fieldset.form',
                 m('fieldset',

lib/VNWeb/TableOpts.pm
@@ -217,6 +217,8 @@ sub sql_order {
     $sql =~ /[?!]o/ ? ($sql =~ s/\?o/$order/rg =~ s/!o/$opposite_order/rg) : "$sql $order";
 }
 
+sub ORDER { RAW $_[0]->sql_order }
+
 
 # Returns whether the given column key is visible.
 sub vis { my $c = $_[0][1]{columns}{$_[1]}; $c && defined $c->{vis_id} && ($_[0][0] & (1 << (12+$c->{vis_id}))) }

lib/VNWeb/VN/Length.pm
@@ -44,8 +44,12 @@ sub listing_($opt, $url, $count, $list, $mode) {
                 td_ class => 'tc3'.($_->{ignore}?' grayedout':''), sub { vnlength_ $_->{length} };
                 td_ class => 'tc4'.($_->{ignore}?' grayedout':''), ['Slow','Normal','Fast','-']->[$_->{speed}//3];
                 td_ class => 'tc5', sub {
-                    my %l = map +($_,1), map $_->{lang}->@*, $_->{rel}->@*;
-                    abbr_ class => "icon-lang-$_", title => $LANGUAGE{$_}{txt}, '' for sort keys %l;
+                    if ($_->{lang}) {
+                        abbr_ class => "icon-lang-$_", title => $LANGUAGE{$_}{txt}, '' for sort $_->{lang}->@*;
+                    } else {
+                        my %l = map +($_,1), map $_->{lang}->@*, $_->{rel}->@*;
+                        abbr_ class => "icon-lang-$_", title => $LANGUAGE{$_}{txt}, '' for sort keys %l;
+                    }
                     join_ ',', sub { a_ href => "/$_->{id}", $_->{id} }, sort { idcmp $a->{id}, $b->{id} } $_->{rel}->@*;
                 };
                 td_ class => 'tc6'.($_->{ignore}?' grayedout':''), sub {
@@ -113,25 +117,26 @@ FU::get qr{/(?:$RE{vid}/|$RE{uid}/)?lengthvotes}, sub($vid=undef, $uid=undef) {
 
     my sub url { '?'.query_encode({%$opt, @_}) }
 
-    my $where = sql_and
-        $mode ? sql($mode eq 'v' ? 'l.vid =' : 'l.uid =', \$o->{id}) : (),
+    my $where = AND
+        $mode ? SQL($mode eq 'v' ? 'l.vid =' : 'l.uid =', $o->{id}) : (),
         $mode eq 'u' && auth && $o->{id} eq auth->uid ? () : 'NOT l.private',
-        defined $opt->{ign} ? sql('l.speed IS', $opt->{ign} ? 'NULL' : 'NOT NULL') : ();
-    my $count = fu->dbVali('SELECT COUNT(*) FROM vn_length_votes l WHERE', $where);
+        defined $opt->{ign} ? SQL('l.speed IS', $opt->{ign} ? 'NULL' : 'NOT NULL') : ();
+    my $count = fu->SQL('SELECT COUNT(*) FROM vn_length_votes l', WHERE $where)->val;
 
-    my $lst = fu->dbPagei({results => $opt->{s}->results, page => $opt->{p}},
-      'SELECT l.id, l.uid, l.vid, l.length, l.speed, l.notes, l.private, l.rid::text[] AS rel, '
-            , sql_totime('l.date'), 'AS date, u.perm_lengthvote IS NOT DISTINCT FROM false AS ignore',
-              $mode ne 'u' ? (', ', sql_user()) : (),
+    my $lst = fu->SQL(
+      'SELECT l.id, l.uid, l.vid, l.length, l.speed, l.notes, l.private, l.rid AS rel, l.lang, l.date
+            , u.perm_lengthvote IS NOT DISTINCT FROM false AS ignore',
+              $mode ne 'u' ? (', ', USER) : (),
               $mode ne 'v' ? ', v.title' : (), '
          FROM vn_length_votes l
          LEFT JOIN users u ON u.id = l.uid',
-         $mode ne 'v' ? ('JOIN', vnt, 'v ON v.id = l.vid') : (),
-       'WHERE', $where,
-       'ORDER BY', $opt->{s}->sql_order(),
-    );
+         $mode ne 'v' ? ('JOIN', VNT, 'v ON v.id = l.vid') : (),
+        WHERE($where),
+       'ORDER BY', $opt->{s}->ORDER,
+       'LIMIT', $opt->{s}->results, 'OFFSET', $opt->{s}->results*($opt->{p}-1),
+    )->allh;
     $_->{rel} = [ map +{ id => $_ }, $_->{rel}->@* ] for @$lst;
-    enrich_flatten lang => id => id => 'SELECT id, lang FROM releases_titles WHERE id IN', map $_->{rel}, @$lst;
+    fu->enrich(aov => 'lang', 'SELECT id, lang FROM releases_titles WHERE id', [ map $_->{rel}->@*, @$lst ]);
 
     my $title = 'Length votes'.($mode ? ($mode eq 'v' ? ' for ' : ' by ').$o->{title}[1] : '');
     framework_ title => $title, dbobj => $o, sub {
@@ -189,6 +194,7 @@ my($FORM_IN, $FORM_OUT) = form_compile 'in', 'out', {
     vid      => { vndbid => 'v' },
     vote     => { type => 'hash', default => undef, keys => {
         rid      => { minlength => 1, elems => { vndbid => 'r' } },
+        lang     => { minlength => 1, elems => { enum => \%LANGUAGE } },
         length   => { uint => 1, range => [1,26159] }, # 435h59m, largest round-ish number where the 'fast' speed adjustment doesn't overflow a smallint
         speed    => { default => undef, uint => 1, enum => [0,1,2] },
         private  => { anybool => 1 },
@@ -200,24 +206,26 @@ my($FORM_IN, $FORM_OUT) = form_compile 'in', 'out', {
 };
 
 
-FU::get qr{/$RE{vid}/lengthvote}, sub($id) {
-    my $v = fu->dbRowi('SELECT id, title[1+1], devstatus FROM', vnt, 'v WHERE NOT hidden AND id =', \$id);
-    fu->notfound if !$v->{id};
-    fu->denied if !can_vote;
-
-    my $my = fu->dbRowi('SELECT rid::text[] AS rid, length, speed, private, notes FROM vn_length_votes WHERE vid =', \$v->{id}, 'AND uid =', \auth->uid);
-
-    my $today = strftime '%Y%m%d', gmtime;
-    my $rels = [ grep $_->{released} <= $today, releases_by_vn($v->{id})->@* ];
+sub available_releases($v) {
+    my $rels = releases_by_vn $v->{id}, released => 1;
     my $hasnontrial = grep $_->{rtype} ne 'trial', @$rels;
     # Voting on trials is only allowed if development has been cancelled and there's no other release.
-    $rels = [ grep $_->{rtype} ne 'trial' || ($v->{devstatus} == 2 && !$hasnontrial), @$rels ];
+    [ grep $_->{rtype} ne 'trial' || ($v->{devstatus} == 2 && !$hasnontrial), @$rels ];
+}
+
+
+FU::get qr{/$RE{vid}/lengthvote}, sub($id) {
+    my $v = fu->SQL('SELECT id, title[2], devstatus FROM', VNT, 'v WHERE NOT hidden AND id =', $id)->rowh || fu->notfound;
+    fu->denied if !can_vote;
+
+    my $my = fu->SQL('SELECT rid, lang, length, speed, private, notes FROM vn_length_votes WHERE vid =', $v->{id}, 'AND uid =', auth->uid)->rowh;
+    my $rels = available_releases $v;
 
     framework_ title => "Edit play time for $v->{title}", sub {
-        if (@$rels || $my->{rid}) {
+        if (@$rels || $my) {
             div_ widget('VNLengthVote', $FORM_OUT, {
                 vid      => $v->{id},
-                vote     => $my->{rid} ? $my : undef,
+                vote     => $my,
                 title    => $v->{title},
                 maycount => $v->{devstatus} != 1,
                 releases => $rels,
@@ -239,19 +247,29 @@ js_api VNLengthVote => $FORM_IN, sub ($data) {
     fu->denied if !can_vote;
     my %where = ( uid => auth->uid, vid => $data->{vid} );
 
-    if ($data->{vote}) {
-        my %fields = (
-            $data->{vote}->%*,
-            rid => sql sql_array($data->{vote}{rid}->@*), '::vndbid[]'
-        );
-        $fields{speed} = undef if $fields{private};
-        fu->dbExeci(
-            'INSERT INTO vn_length_votes', { %where, %fields },
-            'ON CONFLICT (uid, vid) DO UPDATE SET', \%fields
-        );
-    } else {
-        fu->dbExeci('DELETE FROM vn_length_votes WHERE', \%where);
+    if (!$data->{vote}) {
+        fu->SQL('DELETE FROM vn_length_votes', WHERE \%where)->exec;
+        return {};
     }
+
+    my $v = fu->SQL('SELECT id, devstatus FROM', VNT, 'v WHERE NOT hidden AND id =', $data->{vid})->rowh || fu->notfound;
+    my $rels = available_releases $v;
+    my %rels = map +($_->{id},$_), @$rels;
+
+    my %fields = $data->{vote}->%*;
+    $fields{rid} = [ grep $rels{$_}, $fields{rid}->@* ];
+    return 'No valid releases selected' if !$fields{rid}->@*;
+
+    my %langs = map +($_,1), map $rels{$_}{lang}->@*, $fields{rid}->@*;
+    $fields{lang} = [ grep $langs{$_}, $fields{lang}->@* ];
+    return 'No valid language selected' if !$fields{lang}->@*;
+
+    $fields{speed} = undef if $fields{private};
+
+    fu->SQL(
+        'INSERT INTO vn_length_votes', VALUES({ %where, %fields }),
+        'ON CONFLICT (uid, vid) DO UPDATE', SET \%fields
+    )->exec;
     +{};
 };
 

sql/schema.sql
@@ -1606,7 +1606,8 @@ CREATE TABLE vn_length_votes (
   speed      smallint, -- [pub] NULL=uncounted/ignored, 0=slow, 1=normal, 2=fast
   private    boolean NOT NULL,
   rid        vndbid[] NOT NULL, -- [pub]
-  notes      text NOT NULL DEFAULT '' -- [pub]
+  notes      text NOT NULL DEFAULT '', -- [pub]
+  lang       language[] -- [pub] NULL for votes before 2025-06-05, inferred from the release language(s) in that case
 );
 
 -- vn_image_votes

util/updates/2025-06-05-vn-length-votes-lang.sql
@@ -1,0 +1,1 @@
+ALTER TABLE vn_length_votes ADD COLUMN lang language[];

commit de900f3a6dbc4429b509f8dfd9ea34bdab8c77e2

JS/DS: Fix dropdown closing for AdvSearch search input fields
Fixes https://vndb.org/t2520.985
Broken in 4cae2dcc55d232c4219c5365e645d7f904b2c700

js/basic/ds.js
@@ -111,7 +111,7 @@ class DS {
         this.source = source;
         if (source && source.opts) Object.assign(this, source.opts);
         if (opts) Object.assign(this, opts);
-        if (!('keep' in this)) this.keep = this.checked;
+        if (!('keep' in this) && ('checked' in this)) this.keep = this.checked;
         this.open = this.open.bind(this);
         this.list = [];
     }