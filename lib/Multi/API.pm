xso4/vndb-mirror

commit 3c085a61aef23154b027bc110b6ac9e9b78cd410

SQL: Merge chars.b_month and b_day
A silly change, perhaps, but I didn't see a reason why these should be
separate. Passing them around as a single field is a little easier.

lib/Multi/API.pm
@@ -930,12 +930,12 @@ my %GET_CHARACTER = (
   },
   flags  => {
     basic => {
-      select => 'c.title[2] AS name, c.title[4] AS original, c.sex AS gender, c.spoil_sex AS spoil_gender, c.bloodt, c.b_day, c.b_month',
+      select => 'c.title[2] AS name, c.title[4] AS original, c.sex AS gender, c.spoil_sex AS spoil_gender, c.bloodt, c.birthday',
       proc => sub {
         $_[0]{original} = undef if $_[0]{original} eq $_[0]{name};
         $_[0]{gender}   = undef if !$_[0]{gender};
         $_[0]{bloodt}   = undef if $_[0]{bloodt} eq 'unknown';
-        $_[0]{birthday} = [ delete($_[0]{b_day})*1||undef, delete($_[0]{b_month})*1||undef ];
+        $_[0]{birthday} = [ int($_[0]{birthday}/100)||undef, ($_[0]{birthday}%100)||undef ];
       },
     },
     details => {

lib/VNWeb/API.pm
@@ -940,7 +940,7 @@ api_query '/character',
         hips     => { select => 'c.s_hip AS hips', @NINT },
         cup      => { select => 'c.cup_size AS cup', @NSTR },
         age      => { select => 'c.age' },
-        birthday => { select => 'CASE WHEN c.b_month = 0 THEN NULL ELSE ARRAY[c.b_month, NULLIF(c.b_day, 0)]::int[] END AS birthday' },
+        birthday => { select => 'c.birthday', proc => sub { $_[0] = $_[0] ? [ int $_[0]/100, $_[0]%100 ] : undef } },
         sex      => { select => "NULLIF(ARRAY[NULLIF(c.sex, ''), NULLIF(COALESCE(c.spoil_sex, c.sex), '')]::text[], '{NULL,NULL}') AS sex" },
         vns      => {
             enrich => sub { sql 'SELECT cv.id AS cid, v.id', $_[0], 'FROM chars_vns cv JOIN vnt v ON v.id = cv.vid', $_[1], 'WHERE NOT v.hidden AND cv.id IN', $_[2] },

js/contrib/CharEdit.js
@@ -68,14 +68,18 @@ const GenInfo = vnode => {
         m('fieldset',
             m('label[for=bmonth]', 'Birthday'),
             m(Select, {
-                id: 'bmonth', data, field: 'b_month',
+                id: 'bmonth',
                 options: range(0, 12).map(m => [m, m ? m + ' (' + RDate.months[m-1] + ')' : 'Unknown']),
+                value: Math.trunc(data.birthday / 100),
+                oninput: v => data.birthday = v * 100 + (v === 0 ? 0 : data.birthday % 100),
             }),
-            data.b_month === 0 ? null : m(Select, {
-                data, field: 'b_day', class: 'sw',
+            data.birthday === 0 ? null : m(Select, {
+                class: 'sw',
                 options: range(0, 31).map(m => [m,m === 0 ? 'day' : m]),
+                value: data.birthday % 100,
+                oninput: v => data.birthday = Math.trunc(data.birthday / 100)*100 + v,
             }),
-            data.b_month !== 0 && data.b_day === 0 ? m('p.invalid', 'Day is required.') : null,
+            data.birthday !== 0 && (data.birthday % 100) === 0 ? m('p.invalid', 'Day is required.') : null,
         ),
         m('fieldset',
             m('label[for=age]', 'Age'),

lib/VNDB/Func.pm
@@ -20,7 +20,7 @@ our @EXPORT = (qw|
     imgsize
     norm_ip
     minage
-    fmtvote fmtmedia fmtage fmtdate fmtrating fmtspoil fmtanimation
+    fmtvote fmtmedia fmtage fmtdate fmtrating fmtspoil fmtanimation fmtbirthday
     rdate
     imgpath imgurl thumburl imgiv
     tlang tattr
@@ -230,6 +230,12 @@ sub fmtanimation($a, $cat) {
         $a & 16 ? '3D' : (),
         $a & 32 ? 'Live action' : ()
     ).($cat ? " $cat" : '');
+}
+
+
+sub fmtbirthday($b) {
+    state @m = qw{January February March April May June July August September October November December};
+    $b ? sprintf '%d %s', $b%100, $m[int($b/100)-1] : ''
 }
 
 

lib/VNWeb/AdvSearch.pm
@@ -431,7 +431,7 @@ f c => 12 => 'age',        { default => undef, uint => 1, max => 32767 },
 f c => 13 => 'trait',      { type => 'any', func => \&_validate_trait }, compact => \&_compact_trait, sql_list => _sql_where_trait('traits_chars', 'cid');
 f c => 15 => 'dtrait',     { type => 'any', func => \&_validate_trait }, compact => \&_compact_trait, sql_list => _sql_where_trait('chars_traits', 'id');
 f c => 14 => 'birthday',   { default => [0,0], type => 'array', length => 2, values => { uint => 1, max => 31 } },
-    '=' => sub { sql 'c.b_month =', \$_->[0], $_->[1] ? ('AND c.b_day =', \$_->[1]) : () };
+    '=' => sub { $_->[1] ? sql 'c.birthday =', \($_->[0]*100 + $_->[1]) : sql 'c.birthday BETWEEN', \($_->[0]*100), 'AND', \($_->[0]*100 + 99) };
 
 # XXX: When this field is nested inside a VN query, it may match seiyuu linked to other VNs.
 # This can be trivially fixed by adding an (AND vs.id = v.id) clause, but that results in extremely slow queries that I've no clue how to optimize.

lib/VNWeb/Chars/Edit.pm
@@ -15,8 +15,7 @@ my($FORM_IN, $FORM_OUT) = form_compile 'in', 'out', {
     spoil_sex  => { default => sub { $_[0] }, enum => \%CHAR_SEX },
     gender     => { default => sub { $_[0] }, enum => \%CHAR_GENDER },
     spoil_gender=>{ default => sub { $_[0] }, enum => \%CHAR_GENDER },
-    b_month    => { default => 0, uint => 1, range => [ 0, 12 ] },
-    b_day      => { default => 0, uint => 1, range => [ 0, 31 ] },
+    birthday   => { default => 0, uint => 1, regex => qr/^([1-9]|1[012])([012][0-9]|3[012])$/ },
     age        => { default => undef, uint => 1, range => [ 0, 32767 ] },
     s_bust     => { default => 0, uint => 1, range => [ 0, 32767 ] },
     s_waist    => { default => 0, uint => 1, range => [ 0, 32767 ] },
@@ -138,7 +137,6 @@ js_api CharEdit => $FORM_IN, sub ($data,@) {
         $data->{locked} = $e->{locked}||0;
     }
     $data->{description} = bb_subst_links $data->{description};
-    ($data->{b_month}, $data->{b_day}) = (0,0) if !$data->{b_day} || !$data->{b_month};
 
     $data->{spoil_sex} //= $data->{sex} if defined $data->{spoil_gender};
     my $sex = ($data->{sex}//'-').($data->{spoil_sex}//'-');

lib/VNWeb/Chars/Page.pm
@@ -66,7 +66,7 @@ sub enrich_item {
 sub fetch_chars {
     my($vid, $where) = @_;
     my $l = tuwf->dbAlli('
-        SELECT id, title, alias, description, sex, spoil_sex, gender, spoil_gender, b_month, b_day
+        SELECT id, title, alias, description, sex, spoil_sex, gender, spoil_gender, birthday
              , s_bust, s_waist, s_hip, height, weight, bloodt, cup_size, age, image
           FROM', charst, 'c WHERE NOT hidden AND (', $where, ')
          ORDER BY sorttitle
@@ -109,8 +109,7 @@ sub _rev_ {
         [ spoil_sex  => 'Sex (spoiler)', empty => undef, fmt => \%CHAR_SEX ],
         [ gender     => 'Gender identity', empty => undef, fmt => \%CHAR_GENDER ],
         [ spoil_gender=>'Gender (spoiler)',empty => undef, fmt => \%CHAR_GENDER ],
-        [ b_month    => 'Birthday/month',empty => 0 ],
-        [ b_day      => 'Birthday/day',  empty => 0 ],
+        [ birthday   => 'Birthday',      empty => 0, fmt => sub { txt_ fmtbirthday $_ } ],
         [ s_bust     => 'Bust',          empty => 0 ],
         [ s_waist    => 'Waist',         empty => 0 ],
         [ s_hip      => 'Hips',          empty => 0 ],
@@ -187,8 +186,8 @@ sub chartable_ {
 
             tr_ sub {
                 td_ class => 'key', 'Birthday';
-                td_ $c->{b_day}.' '.[qw{January February March April May June July August September October November December}]->[$c->{b_month}-1];
-            } if $c->{b_day} && $c->{b_month};
+                td_ fmtbirthday $c->{birthday};
+            } if $c->{birthday};
 
             tr_ sub {
                 td_ class => 'key', 'Age';

sql/schema.sql
@@ -237,8 +237,7 @@ CREATE TABLE chars ( -- dbentry_type=c
   s_bust       smallint NOT NULL DEFAULT 0, -- [pub] cm
   s_waist      smallint NOT NULL DEFAULT 0, -- [pub] cm
   s_hip        smallint NOT NULL DEFAULT 0, -- [pub] cm
-  b_month      smallint NOT NULL DEFAULT 0, -- [pub] Birthday month, 1-12
-  b_day        smallint NOT NULL DEFAULT 0, -- [pub] Birthday day, 1-32
+  birthday     smallint NOT NULL DEFAULT 0, -- [pub] Birthday, 0 or mmdd
   height       smallint NOT NULL DEFAULT 0, -- [pub] cm
   weight       smallint, -- [pub] kg
   main_spoil   smallint NOT NULL DEFAULT 0, -- [pub]
@@ -266,8 +265,7 @@ CREATE TABLE chars_hist (
   s_bust       smallint NOT NULL DEFAULT 0, -- cf=Measurements
   s_waist      smallint NOT NULL DEFAULT 0, -- cf=Measurements
   s_hip        smallint NOT NULL DEFAULT 0, -- cf=Measurements
-  b_month      smallint NOT NULL DEFAULT 0, -- cf=Age
-  b_day        smallint NOT NULL DEFAULT 0, -- cf=Age
+  birthday     smallint NOT NULL DEFAULT 0, -- cf=Age
   height       smallint NOT NULL DEFAULT 0, -- cf=Measurements
   weight       smallint, -- cf=Measurements
   main_spoil   smallint NOT NULL DEFAULT 0, -- cf=Main

commit d5573b8f6d6eed5796bb70e4a6bd6e8c9430f7e4

APIv1: Fix day/month order in character birthday response
Broken back in 3c085a61aef23154b027bc110b6ac9e9b78cd410

lib/Multi/API.pm
@@ -940,7 +940,7 @@ my %GET_CHARACTER = (
         $_[0]{original} = undef if $_[0]{original} eq $_[0]{name};
         $_[0]{gender}   = undef if !$_[0]{gender};
         $_[0]{bloodt}   = undef if $_[0]{bloodt} eq 'unknown';
-        $_[0]{birthday} = [ int($_[0]{birthday}/100)||undef, ($_[0]{birthday}%100)||undef ];
+        $_[0]{birthday} = [ ($_[0]{birthday}%100)||undef, int($_[0]{birthday}/100)||undef ];
       },
     },
     details => {