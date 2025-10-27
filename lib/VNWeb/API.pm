xso4/vndb-mirror

commit 29298f1935b32a4e97b9552e6e448a7e5e2822ee

Characters: Support multilanguage names + romanized & spoiler aliases

lib/VNWeb/API.pm
@@ -914,12 +914,13 @@ api_query '/character',
     search => [ 'c', 'c.id' ],
     joins => {
         image => 'LEFT JOIN images i ON i.id = c.image',
+        alias => "LEFT JOIN (SELECT id, string_agg(name || coalesce(E'\\n'||latin, ''), E'\\n') FROM chars_alias GROUP BY id) ca(id,aliases) ON ca.id = c.id",
     },
     fields => {
         id       => {},
         name     => { select => 'c.title[2] AS name' },
         original => { ALTTITLE 'c.title', 'original' },
-        aliases  => { select => 'c.alias AS aliases', @MSTR },
+        aliases  => { join => 'alias', select => "coalesce(ca.aliases, '') AS aliases", @MSTR },
         description => { select => 'c.description', @NSTR },
         image => {
             fields => { IMG 'c.image', 'image', 'i.' },

lib/Multi/API.pm
@@ -944,9 +944,9 @@ my %GET_CHARACTER = (
       },
     },
     details => {
-      select => 'c.alias AS aliases, c.image, i.c_sexual_avg, i.c_violence_avg, i.c_votecount, i.width AS image_width, i.height AS image_height, c.description, c.age',
+      select => 'c.image, i.c_sexual_avg, i.c_violence_avg, i.c_votecount, i.width AS image_width, i.height AS image_height, c.description, c.age',
       proc => sub {
-        $_[0]{aliases}     ||= undef;
+        $_[0]{aliases}     = undef;
         $_[0]{description} ||= undef;
         $_[0]{image}       = $_[0]{image} ? imgurl $_[0]{image} : undef;
         $_[0]{image_flagging} = image_flagging $_[0]{image}, $_[0];

css/charedit.css
@@ -20,3 +20,7 @@
     tr:not(:first-child).top td { padding-top: 15px }
     tr.opt td { padding-left: 60px }
 }
+
+.chare_names {
+    .tc_name, .tc_name input { width: 200px }
+}

css/v2.css
@@ -611,15 +611,19 @@ div#vntags                    { margin: 0 30px 0 30px; border-top: 1px solid var
 p.chardetailopts { margin: -10px auto 7px auto; width: 800px; text-align: right }
 p.chardetailopts a { margin: 0 5px }
 p.chardetailopts a:last-child { margin: 0 0 0 5px }
-div.chardetails { margin: 0 auto; width: 800px; }
 div.charimg     { float: left; width: 256px; margin: 0 15px 0 0; text-align: center }
 div.charimg p   { text-align: center; padding: 0px; margin: 0; }
 .chardesc h2    { margin: 0; }
 .chardesc p     { padding: 0 0 0 5px; }
-div.chardetails table        { float: left; width: 525px; }
-div.chardetails table td.key { width: 106px; }
-div.chardetails.charsep      { margin-top: 30px }
-div.chardetails .lie { text-decoration: line-through }
+div.chardetails {
+    margin: 0 auto; width: 800px;
+    &.charsep     { margin-top: 30px }
+    > table       { float: left; width: 525px; }
+    td.key        { width: 106px; }
+    .names td     { padding: 0 5px }
+    .names td:first-child { padding-left: 0 }
+    .lie          { text-decoration: line-through }
+}
 div.charquotes { margin: 0 auto; width: 800px; }
 
 

js/contrib/CharEdit.js
@@ -1,3 +1,85 @@
+const Names = vnode => {
+    const data = vnode.attrs.data;
+    let idx = 0;
+    data.names.forEach(a => a._idx = ++idx);
+    // XXX: Doesn't update when VNs are added/removed in the form, but that's not a common action.
+    const langs = new Set(data.vnstate.flatMap(v => v.rels.flatMap(r => r.lang)));
+
+    const add = new DS(
+        {
+            opts: { width: 250 },
+            list: (src, str, cb) => DS.ScriptLang.list(src, str, lst => cb(lst.filter(o => langs.has(o.id)))),
+            view: DS.ScriptLang.view
+        }, {
+            onselect: obj => data.names.push({ lang: obj.id, name: '', latin: '', _new: true, _idx: ++idx }),
+            props: obj => data.names.some(n => n.lang === obj.id) ? { selectable: false, append: m('small', ' (already listed)') } : {},
+        }
+    );
+    return { view: vnode => m('fieldset',
+        m('label', 'Name(s)'),
+        m('table.chare_names',
+            m('thead', m('tr',
+                m('td'),
+                m('td.tc_name', 'Name (original script)'),
+                m('td.tc_name', 'Romanization'),
+                m('td'),
+            )),
+            m('tbody', data.names.map(a => m('tr', {key: a._idx},
+                m('td', LangIcon(a.lang)),
+                m('td.tc_name', m(Input, { data: a, field: 'name', maxlength: 100, required: true, focus: a._new })),
+                m('td.tc_name', !a.latin && !mayRomanize.test(a.name) ? null : m(Input, {
+                    data: a, field: 'latin', required: mustRomanize.test(a.name), maxlength: 100,
+                    invalid: a.latin === a.name || mustRomanize.test(a.latin) ? 'Romanization should only contain characters in the latin alphabet.' : null,
+                })),
+                m('td',
+                    // Empty 'langs' can happen when the VN has no releases. The default olang name entry is still usable.
+                    langs.size ? m(Button.Del, { onclick: () => data.names = data.names.filter(x => x !== a) }) : null
+                ),
+            ))),
+            [...langs.values()].some(l => !data.names.some(n => n.lang === l))
+            ?  m('tfoot', m('tr', m('td[colspan=3]', m(DS.Button, { ds: add }, 'Add name')))) : null
+        )
+    )};
+};
+
+const Alias = vnode => {
+    const data = vnode.attrs.data;
+    let idx = 0;
+    data.alias.forEach(a => a._idx = ++idx);
+    return { view: vnode => m('fieldset',
+        m('label', 'Aliases'),
+        m('table.chare_names',
+            m('thead', m('tr',
+                m('td.tc_name', 'Name (original script)'),
+                m('td.tc_name', 'Romanization'),
+                m('td', ''),
+            )),
+            m('tbody', data.alias.map(a => m('tr', {key: a._idx},
+                m('td.tc_name',
+                    m(Input, { data: a, field: 'name', maxlength: 100, required: true, focus: a._new }),
+                    a.name !== '' && data.names.some(n => n.name === a.name || n.latin === a.name) ? m('p.invalid', 'Already listed under Name(s)') : null,
+                ),
+                m('td.tc_name', !a.latin && !mayRomanize.test(a.name) ? null : m(Input, {
+                    /* Many old entries are missing romanization - let's not force people to romanize existing aliases */
+                    data: a, field: 'latin', required: a._new && mustRomanize.test(a.name), maxlength: 100,
+                    invalid: a.latin === a.name || mustRomanize.test(a.latin) ? 'Romanization should only contain characters in the latin alphabet.' : null,
+                })),
+                m('td',
+                    m(Select, { data: a, field: 'spoil', options: spoilLevels }),
+                    m(Button.Del, { onclick: () => data.alias = data.alias.filter(x => x !== a) }),
+                ),
+            ))),
+            m('tfoot', m('tr', m('td[colspan=3]',
+                data.alias.anyDup(({name,latin}) => [name,latin===''?null:latin])
+                ? m('p.invalid', 'There are duplicate aliases.') : null,
+                data.alias.length > 25 ? null : m('button[type=button]', { onclick: () => data.alias.push({
+                    name: '', latin: '', spoil: 0, _idx: ++idx, _new: true
+                }) }, 'Add alias'),
+            )))
+        )
+    )};
+};
+
 const GenInfo = vnode => {
     const data = vnode.attrs.data;
 
@@ -19,28 +101,8 @@ const GenInfo = vnode => {
     });
 
     const view = () => [ m('fieldset.form',
-        m('fieldset',
-            m('label[for=name]', 'Name (original)'),
-            m(Input, {
-                id: 'name', class: 'lw', maxlength: 200, required: true, data, field: 'name',
-            }),
-        ),
-        !data.latin && !mayRomanize.test(data.name) ? null : m('fieldset',
-            m('label[for=latin]', 'Name (latin)'),
-            m(Input, {
-                id: 'latin', class: 'lw', maxlength: 200, data, field: 'latin', placeholder: 'Romanization',
-                required: mustRomanize.test(data.name),
-                invalid: data.latin === data.name || mustRomanize.test(data.latin) ? 'Romanization should only contain characters in the latin alphabet.' : null,
-            }),
-        ),
-        m('fieldset',
-            m('label[for=alias]', 'Aliases'),
-            m(Input, {
-                id: 'alias', class: 'lw', maxlength: 500, data, field: 'alias', type: 'textarea', rows: 3,
-            }),
-            data.alias.match(/,/) ? m('p', 'Reminder: one alias per line!') : null,
-            m('p', '(Un)official aliases, separated by a newline. Must not include spoilers!'),
-        ),
+        m(Names, {data}),
+        m(Alias, {data}),
         m('fieldset',
             m('label[for=description]', 'Description'),
             m(TextPreview, {

lib/VNWeb/Chars/Edit.pm
@@ -7,9 +7,16 @@ use VNWeb::Releases::Lib;
 
 my($FORM_IN, $FORM_OUT) = form_compile 'in', 'out', {
     id         => { default => undef, vndbid => 'c' },
-    name       => { sl => 1, maxlength => 200 },
-    latin      => { default => undef, sl => 1, maxlength => 200 },
-    alias      => { default => '', maxlength => 500 },
+    names      => { sort_keys => 'lang', minlength => 1, aoh => {
+        lang    => { enum => \%LANGUAGE },
+        name    => { sl => 1, maxlength => 100 },
+        latin   => { default => undef, sl => 1, maxlength => 100 },
+    } },
+    alias      => { sort_keys => 'name', maxlength => 25, aoh => {
+        spoil   => { uint => 1, range => [0,2] },
+        name    => { sl => 1, maxlength => 100 },
+        latin   => { default => undef, sl => 1, maxlength => 100 },
+    } },
     description=> { default => '', maxlength => 5000 },
     sex        => { default => '', enum => \%CHAR_SEX },
     spoil_sex  => { default => sub { $_[0] }, enum => \%CHAR_SEX },
@@ -105,10 +112,11 @@ FU::get qr{/$RE{crev}/(edit|copy)} => sub($id, $rev, $action) {
 
 FU::get qr{/$RE{vid}/addchar}, sub($vid) {
     fu->denied if !can_edit c => undef;
-    my $title = fu->SQL('SELECT title[2] FROM', VNT, 'WHERE NOT hidden AND id =', $vid)->val;
+    my($title, $olang) = fu->SQL('SELECT title[2], olang FROM', VNT, 'WHERE NOT hidden AND id =', $vid)->rowl;
     fu->notfound if !length $title;
 
     my $e = $FORM_OUT->empty;
+    $e->{names} = [{ name => '', latin => '', lang => $olang }];
     $e->{vns} = [{ vid => $vid, rid => undef, spoil => 0, role => 'primary' }];
     $e->{vnstate} = [{
         id => $vid,

lib/VNWeb/Chars/Page.pm
@@ -58,11 +58,18 @@ sub enrich_item($c) {
 # Also used by Chars::VNTab.
 sub fetch_chars($vid, $where) {
     my $l = fu->SQL('
-        SELECT id, title, alias, description, sex, spoil_sex, gender, spoil_gender, birthday
+        SELECT id, title, description, sex, spoil_sex, gender, spoil_gender, birthday
              , s_bust, s_waist, s_hip, height, weight, bloodt, cup_size, age, image
           FROM', CHARST, 'c WHERE NOT hidden AND (', $where, ')
          ORDER BY sorttitle
     ')->allh;
+
+    fu->enrich(aoh => 'names', sub {
+        SQL 'SELECT id, lang, name, latin FROM chars_names WHERE id', IN $_, 'ORDER BY lang'
+    }, $l);
+    fu->enrich(aoh => 'alias', sub {
+        SQL 'SELECT id, name, latin, spoil FROM chars_alias WHERE id', IN $_, 'ORDER BY COALESCE(latin, name)'
+    }, $l);
 
     fu->enrich(aoh => 'vns', sub { SQL '
         SELECT cv.id, cv.vid, cv.rid, cv.spoil, cv.role, v.title, r.title AS rtitle
@@ -92,9 +99,11 @@ sub fetch_chars($vid, $where) {
 
 sub _rev_($c) {
     revision_ $c, \&enrich_item,
-        [ name       => 'Name'           ],
-        [ latin      => 'Name (latin)'   ],
-        [ alias      => 'Aliases'        ],
+        [ names      => 'Name',          txt => sub { "[$_->{lang}] $_->{name}".(length $_->{latin} ? " / $_->{latin}" : '') } ],
+        [ alias      => 'Aliases',       txt => sub {
+            $_->{name}.(length $_->{latin} ? " / $_->{latin}" : '')
+            .['', ' [minor spoiler]', ' [major spoiler]']->[$_->{spoil}]
+        } ],
         [ description=> 'Description'    ],
         [ sex        => 'Sex',           fmt => \%CHAR_SEX ],
         [ spoil_sex  => 'Sex (spoiler)', empty => undef, fmt => \%CHAR_SEX ],
@@ -161,9 +170,26 @@ sub chartable_($c, $link=undef, $sep=undef, $vn=undef) {
             }}};
 
             tr_ sub {
-                td_ class => 'key', 'Aliases';
-                td_ $c->{alias} =~ s/\n/, /rg;
-            } if $c->{alias};
+                td_ class => 'key', 'Names';
+                td_ sub { table_ class => 'names', sub {
+                    tr_ class => 'nostripe', sub {
+                        td_ sub { abbr_ class => "icon-lang-$_->{lang}", title => $LANGUAGE{$_->{lang}}{txt}, '' };
+                        td_ tlang($_->{lang}, $_->{name}), $_->{name};
+                        td_ $_->{latin} // '';
+                    } for $c->{names}->@*;
+                } }
+            } if $c->{names}->@* > 1;
+
+            my @alias = grep $_->{spoil} <= $view->{spoilers}, $c->{alias}->@*;
+            tr_ sub {
+                td_ class => 'key', @alias == 1 ? 'Alias' : 'Aliases';
+                td_ sub { table_ class => 'names', sub {
+                    tr_ class => 'nostripe', sub {
+                        td_ sub { txt_ $_->{name}; spoil_ $_->{spoil} };
+                        td_ $_->{latin} // '';
+                    } for @alias;
+                } };
+            } if @alias;
 
             tr_ sub {
                 td_ class => 'key', 'Measurements';
@@ -292,13 +318,14 @@ FU::get qr{/$RE{crev}} => sub($id, $rev=0) {
         $inst_maxspoil||0,
         (map $_->{override}//($_->{lie}?2:$_->{spoil}), grep !$_->{hidden} && !(($_->{override}//0) == 3), $c->{traits}->@*),
         (map $_->{spoil}, $c->{vns}->@*),
+        (map $_->{spoil}, $c->{alias}->@*),
         defined $c->{spoil_sex} || defined $c->{spoil_gender} ? 2 : 0,
         $c->{description} =~ /\[spoiler\]/i ? 2 : 0, # crude
     );
     # Only display the sexual traits toggle when there are sexual traits within the current spoiler level.
     my $has_sex = grep !$_->{hidden} && $_->{sexual} && ($_->{override}//$_->{spoil}) <= $view->{spoilers}, map $_->{traits}->@*, $c, @$inst;
 
-    $c->{title} = titleprefs_swap fu->SQL('SELECT c_lang FROM chars WHERE id =', $c->{id})->val, @{$c}{qw/ name latin /};
+    $c->{title} = titleprefs_obj fu->SQL('SELECT c_lang FROM chars WHERE id =', $c->{id})->val, $c->{names};
     framework_ title => $c->{title}[1], index => !$rev, dbobj => $c, hiddenmsg => 1,
         og => {
             description => bb_format($c->{description}, text => 1),

lib/VNWeb/Chars/VNTab.pm
@@ -11,6 +11,7 @@ sub chars_($v) {
         map max(
             (map $_->{override}//($_->{lie}?2:$_->{spoil}), grep !$_->{hidden} && !(($_->{override}//0) == 3), $_->{traits}->@*),
             (map $_->{spoil}, $_->{vns}->@*),
+            (map $_->{spoil}, $_->{alias}->@*),
             defined $_->{spoil_sex} ? 2 : 0,
             $_->{description} =~ /\[spoiler\]/i ? 2 : 0,
         ), @$chars

commit 59399f4a52fb08d7a6ea6362a39bcfb09c7e5da0

API2: Fix sorting by character name
Broken in 29298f1935b32a4e97b9552e6e448a7e5e2822ee

lib/VNWeb/API.pm
@@ -967,7 +967,7 @@ api_query '/character',
     },
     sort => [
         id       => 'c.id',
-        name     => 'c.name ?o, c.id',
+        name     => 'c.sorttitle ?o, c.id',
     ];
 
 