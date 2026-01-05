ba06f6ed8b8558994cb1723596260f1d6fbe23d0
Yorhel <git@yorhel.nl>
2026-01-05 14:52:48 +0100

    VNTask::EL::AppStore: Attempt to guess country for region-locked apps
    Fixes https://vndb.org/t2520.1012

--- lib/VNTask/EL/AppStore.pm
+++ lib/VNTask/EL/AppStore.pm
@@ -4,15 +4,29 @@ use v5.36;
 use VNTask::ExtLinks;
 
 
-sub try($task, $lnk, $region=0) {
-    my $res = http_get 'https://itunes.apple.com/lookup?id='.$lnk->value . ($region ? '&country='.$lnk->data : '');
+# Bad country code fallback for region-locked apps.
+# Only includes country codes I've actually seen games being region-locked in.
+my %lang2country = (
+    ja         => [ 'jp' ],
+    th         => [ 'th' ],
+    zh         => [ 'ch', 'tw', 'hk' ],
+    'zh-Hans'  => [ 'ch', 'tw', 'hk' ],
+    'zh-Hant'  => [ 'ch', 'tw', 'hk' ],
+    ko         => [ 'kr' ],
+    ru         => [ 'ru' ],
+    es         => [ 'es', 'pe' ],
+);
+
+
+sub try($task, $lnk, $region='') {
+    my $res = http_get 'https://itunes.apple.com/lookup?id='.$lnk->value . ($region ? '&country='.$region : '');
     $res->expect(200);
     my $data = $res->json;
     $res->dead('Not found') if !$data->{resultCount};
 
     $data = $data->{results}[0];
     $lnk->save(
-        data   => $region ? $lnk->data : '',
+        data   => $region,
         price  => $data->{formattedPrice},
         detail => {
             developer => $data->{artistName},
@@ -22,17 +36,30 @@ sub try($task, $lnk, $region=0) {
             released  => $data->{releaseDate},
         },
     );
-    $task->done('Found at %s', $region ? $lnk->data : 'regionless');
+    $task->done('Found at %s', $region || 'regionless');
 }
 
 el_queue 'el/appstore',
     freq   => '60d',
     triage => sub($lnk) { $lnk->site eq 'appstore' },
     sub($task, $lnk) {
-        # Try fetching without the region first, most products aren't region-locked.
-        return if eval { try $task, $lnk, 0; 1 };
-        die $@ if !length $lnk->data;
-        try $task, $lnk, 1;
+        # Try fetching without the region first (implies country=US), most products aren't region-locked.
+        return if eval { try $task, $lnk; 1 };
+
+        # Try fetching with 'data' if we have that.
+        return if $lnk->data && eval { try $task, $lnk, $lnk->data; 1 };
+
+        # If that doesn't work, query the database for possible regions
+        my %regions = map +($_,1), map @$_, grep $_, map $lang2country{$_}, $task->sql('
+            SELECT DISTINCT rt.lang
+              FROM releases_extlinks re
+              JOIN releases r ON r.id = re.id
+              JOIN releases_titles rt ON rt.id = r.id
+             WHERE NOT r.hidden AND re.link = $1', $lnk->id
+        )->flat->@*;
+        delete $regions{ $lnk->data } if $lnk->data;
+        eval { try $task, $lnk, $_; 1 } && return for sort keys %regions;
+        die $@;
     };
 
 1;
